64fb3e71e7bfa98e242491748ee1c23940dcb224
Tomoyuki Aota <tomoyukia@gmail.com>
2019-01-20 03:41:24 +0900

    Test if CI fails. (Incorrect executable path is specified.)

--- test-package.js
+++ test-package.js
@@ -2,22 +2,24 @@ console.log(`Start of "${__filename}"`);
 
 const child_process = require("child_process");
 
+const releaseDirectory = './release';
+
 function getTestInfo() {
     switch(process.platform) {
         case "win32":
             return {
                 executableCreationCommand: "npm run electron:windows",
-                executablePath: "./release/angular-electron 0.0.1.exe"
+                executablePath: `${releaseDirectory}/angular-electron 0.0.1.ex`
             };
         case "darwin":
             return {
                 executableCreationCommand: "npm run electron:mac",
-                executablePath: "./release/angular-electron 0.0.1.exe"
+                executablePath: `${releaseDirectory}/angular-electron 0.0.1.ex`
             };
         case "linux":
             return {
                 executableCreationCommand: "npm run electron:linux",
-                executablePath: "./release/angular-electron 0.0.1.exe"
+                executablePath: `${releaseDirectory}/angular-electron 0.0.1.ex`
             };
         default:
             throw new Error("Unsupported platform for test-package.js");
@@ -32,16 +34,42 @@ function createExecutable(testInfo) {
 }
 
 function printItemsInReleaseDirectory() {
-    const releaseDirectory = './release/';
-    const fs = require('fs'); 
+    console.log("-----------------------------------------------");
+    console.log(`Following items exist in "${releaseDirectory}":`)
+    const fs = require('fs');
     fs.readdirSync(releaseDirectory).forEach(file => {
-      console.log(file);
+      console.log(`  ${file}`);
     })
+    console.log("-----------------------------------------------");
 }
 
 function launchExecutable(testInfo) {
-    console.log(`Launch "${testInfo.executablePath}"`);
-    child_process.execFile(testInfo.executablePath);
+    const executionTime = 30000;
+    console.log(`Launch "${testInfo.executablePath}" and let it run for ${executionTime} ms.`);
+    const executableProcess = child_process.spawn(testInfo.executablePath);
+
+    executableProcess.stdout.on('data', function(data){
+        console.log(`stdout: ${data}`);
+    });
+
+    executableProcess.stderr.on('data', function(data){
+        console.error(`stderr: ${data}`);
+    });
+
+    executableProcess.on('error', (err) => {
+        console.error(`Failed to start ${testInfo.executablePath}`);
+        console.error(err);
+        throw err;
+    });
+
+    executableProcess.on('close', (code) => {
+        console.log(`"${testInfo.executablePath}" is manually killed.`);
+    });
+
+    setTimeout(() => {
+        const kill  = require('tree-kill');
+        kill(executableProcess.pid);
+    }, executionTime);
 }
 
 function runPackageTest() {
