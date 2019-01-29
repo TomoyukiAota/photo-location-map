4d97cddb059c4f309c776c3963797635c6b90b82
Tomoyuki Aota <tomoyukia@gmail.com>
2019-01-29 20:48:00 +0900

    Add sidebar component.

--- src/app/components/sidebar/sidebar.component.spec.ts
+++ src/app/components/sidebar/sidebar.component.spec.ts
@@ -1,24 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { AngularSplitModule } from 'angular-split';
+import { TranslateModule } from '@ngx-translate/core';
 
-import { HomeComponent } from './home.component';
+import { SidebarComponent } from './sidebar.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('SidebarComponent', () => {
+  let component: SidebarComponent;
+  let fixture: ComponentFixture<SidebarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ],
+      declarations: [ SidebarComponent ],
       imports: [
-        AngularSplitModule.forRoot()
+        TranslateModule.forRoot()
       ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
