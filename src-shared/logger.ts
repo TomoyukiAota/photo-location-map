4fed3a9210049ad83f0b07451fac343f9ba8826d
Tomoyuki Aota <tomoyukia@gmail.com>
2019-03-23 05:51:28 +0900

    Write log into file. (Also, test:unit and test:e2e are disabled since they seem working well with Node.js API like fs, os, and path.)

--- src-shared/logger.ts
+++ src-shared/logger.ts
@@ -1,36 +1,75 @@
 /* tslint:disable:no-console */
 
+import * as os from 'os';
+import * as fs from 'fs-extra';
 import * as moment from 'moment-timezone';
+import { LogFileConfig } from '../src-main/log-file-config';
 import { ProcessUtil } from './process-util';
 
-export class Logger {
-    public static error(message: string, ...object: any) {
-        const text = this.generateLogText(message, 'error');
-        console.error(text, ...object);
+class LoggerImpl {
+    private static fileSystem: typeof fs | 'unavailable';
+
+    public static initialize() {
+        if (ProcessUtil.isElectron()) {
+            if (ProcessUtil.isElectronMain()) {
+                LogFileConfig.setup('./log', `${this.dateTimeInBasicFormat()}_photo-location-map_log.txt`);
+                fs.ensureFileSync(LogFileConfig.filePath);
+                this.fileSystem = fs;
+            } else {
+                this.fileSystem = window.require('electron').remote.require('fs');
+            }
+        }
     }
 
-    public static warn(message: string, ...object: any) {
-        const text = this.generateLogText(message, 'warn');
-        console.warn(text, ...object);
+    private static dateTimeInBasicFormat() {
+        return moment.utc().format('YYYYMMDDTHHmmss.SSS[Z]');
     }
 
-    public static info(message: string, ...object: any) {
-        const text = this.generateLogText(message, 'info');
-        console.info(text, ...object);
-    }
-
-    public static debug(message: string, ...object: any) {
-        const text = this.generateLogText(message, 'debug');
-        console.debug(text, ...object);
-    }
-
-    private static dateTime() {
+    private static dateTimeInExtendedFormat() {
         return moment.utc().format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
     }
 
-    private static generateLogText(message: string, level: string): string {
-        const dateTime = this.dateTime();
+    public static generateLogText(message: string, level: string): string {
+        const dateTime = this.dateTimeInExtendedFormat();
         const processType = ProcessUtil.processType();
         return `[${dateTime}] [${processType}] [${level}] ${message}`;
+    }
+
+    public static appendToLogFile(message: string, ...object: any) {
+        if (this.fileSystem === 'unavailable') {
+            // Do nothing because File System API is not avaialble.
+        } else {
+            this.fileSystem.appendFile(LogFileConfig.filePath, message + os.EOL, (err) => {
+                if (err) throw err;
+            });
+        }
+    }
+}
+
+LoggerImpl.initialize();
+
+export class Logger {
+    public static error(message: string, ...object: any) {
+        const text = LoggerImpl.generateLogText(message, 'error');
+        console.error(text, ...object);
+        LoggerImpl.appendToLogFile(text, ...object);
+    }
+
+    public static warn(message: string, ...object: any) {
+        const text = LoggerImpl.generateLogText(message, 'warn');
+        console.warn(text, ...object);
+        LoggerImpl.appendToLogFile(text, ...object);
+    }
+
+    public static info(message: string, ...object: any) {
+        const text = LoggerImpl.generateLogText(message, 'info');
+        console.info(text, ...object);
+        LoggerImpl.appendToLogFile(text, ...object);
+    }
+
+    public static debug(message: string, ...object: any) {
+        const text = LoggerImpl.generateLogText(message, 'debug');
+        console.debug(text, ...object);
+        LoggerImpl.appendToLogFile(text, ...object);
     }
 }
