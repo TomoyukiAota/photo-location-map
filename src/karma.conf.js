a0986670fedfa9038205cce7dad6ca56bb53b3bb
Tomoyuki Aota <tomoyukia@gmail.com>
2019-03-31 03:47:33 +0900

    Introduce karma-mocha-reporter

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-electron')
+      require('karma-electron'),
+      require('karma-mocha-reporter')
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
@@ -22,7 +23,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['mocha', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
