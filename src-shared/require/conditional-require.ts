ab520076116daf5018a961cde13977595f0b17bd
Tomoyuki Aota <tomoyukia@gmail.com>
2019-04-08 00:04:07 +0900

    Fix path separator on Windows.

--- src-shared/require/conditional-require.ts
+++ src-shared/require/conditional-require.ts
@@ -3,6 +3,7 @@ import { ProcessIdentifier } from '../process/process-identifier';
 export class ConditionalRequire {
   public static electron: typeof import('electron') = null;
   public static os: typeof import('os') = null;
+  public static path: typeof import('path') = null;
 
   public static initialize() {
     if (!ProcessIdentifier.isElectron())
@@ -12,10 +13,12 @@ export class ConditionalRequire {
       // For Electron main process, use global.require function.
       this.electron = require('electron');
       this.os = require('os');
+      this.path = require('path');
     } else {
       // For Electron renderer process, use window.require function.
       this.electron = window.require('electron');
       this.os = window.require('os');
+      this.path = window.require('path');
     }
   }
 }
