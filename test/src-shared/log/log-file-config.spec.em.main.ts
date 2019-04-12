0acd676067a717bd050d0e880d9f194a6087153d
Tomoyuki Aota <tomoyukia@gmail.com>
2019-04-13 00:34:11 +0900

    Fix test description.

--- test/src-shared/log/log-file-config.spec.em.main.ts
+++ test/src-shared/log/log-file-config.spec.em.main.ts
@@ -2,16 +2,16 @@ import assert = require('assert');
 import { LogFileConfig } from '../../../src-shared/log/log-file-config';
 
 describe('LogFileConfig (in main process)', () => {
-  it('dirName includes application name and "logs"', () => {
+  it('dirName should include application name and "logs"', () => {
     assert(LogFileConfig.dirName.includes('Photo Location Map') === true);
     assert(LogFileConfig.dirName.includes('logs') === true);
   });
 
-  it('fileName includes photo-location-map_log.txt', () => {
+  it('fileName should include photo-location-map_log.txt', () => {
     assert(LogFileConfig.fileName.includes('photo-location-map_log.txt') === true);
   });
 
-  it('filePath includes dirName and fileName', () => {
+  it('filePath should include dirName and fileName', () => {
     assert(LogFileConfig.filePath.includes(LogFileConfig.dirName) === true);
     assert(LogFileConfig.filePath.includes(LogFileConfig.fileName) === true);
   });
