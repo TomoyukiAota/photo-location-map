8dcdbe915206d6d8f4d91ea5a4865d727675ae2f
Tomoyuki Aota <tomoyukia@gmail.com>
2019-04-14 18:31:46 +0900

    Fix indent.

--- package-test/package-test-logger.js
+++ package-test/package-test-logger.js
@@ -1,20 +1,19 @@
 const moment = require('moment-timezone');
 
-module.exports = class PackageTestLogger
-{
-    static dateTime() {
-        return moment.utc().format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");
-    }
+module.exports = class PackageTestLogger {
+  static dateTime() {
+    return moment.utc().format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");
+  }
 
-    static error(message) {
-        console.error(`[${this.dateTime()}] [error] ${message}`);
-    }
+  static error(message) {
+    console.error(`[${this.dateTime()}] [error] ${message}`);
+  }
 
-    static warn(message) {
-        console.warn(`[${this.dateTime()}] [warn] ${message}`);
-    }
+  static warn(message) {
+    console.warn(`[${this.dateTime()}] [warn] ${message}`);
+  }
 
-    static info(message) {
-        console.info(`[${this.dateTime()}] [info] ${message}`);
-    }
+  static info(message) {
+    console.info(`[${this.dateTime()}] [info] ${message}`);
+  }
 }
