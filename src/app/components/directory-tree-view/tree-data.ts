3eab8b962911998df1fd138a5eafc3373c367cb3
Tomoyuki Aota <tomoyukia@gmail.com>
2019-04-26 02:31:19 +0900

    Extract tree data in a separate file.

---
+++ src/app/components/directory-tree-view/tree-data.ts
@@ -0,0 +1,22 @@
+/**
+ * The data for tree view.
+ */
+export const treeData = {
+  Root: {
+    Groceries: {
+      'Almond Meal flour': null,
+      'Organic eggs': null,
+      'Protein Powder': null,
+      Fruits: {
+        Apple: null,
+        Berries: ['Blueberry', 'Raspberry'],
+        Orange: null
+      }
+    },
+    Reminders: [
+      'Cook dinner',
+      'Read the Material Design spec',
+      'Upgrade Application to Angular'
+    ]
+  }
+};
