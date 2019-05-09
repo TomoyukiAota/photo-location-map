f857036664c2dfb63241676b4536d67f0bdf4911
Tomoyuki Aota <tomoyukia@gmail.com>
2019-05-10 03:06:24 +0900

    Add PhotoDataService.

---
+++ src/app/shared/photo-data.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { PhotoDataService } from './photo-data.service';
+
+describe('PhotoDataService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: PhotoDataService = TestBed.get(PhotoDataService);
+    expect(service).toBeTruthy();
+  });
+});
