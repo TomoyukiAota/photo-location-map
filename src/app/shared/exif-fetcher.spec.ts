af4f3fad2d6e309a28d6bd9d80a8a9b7e75e07de
Tomoyuki Aota <tomoyukia@gmail.com>
2019-05-16 03:20:25 +0900

    Setup electron-mocha renderer-process testing environment.

--- src/app/shared/exif-fetcher.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ExifFetcher } from './exif-fetcher';
-
-describe('ExifFetcher', () => {
-  it('should create an instance', () => {
-    expect(new ExifFetcher()).toBeTruthy();
-  });
-});
--- test/src-shared/environment/environment-detector.spec.em.renderer.ts
+++ test/src-shared/environment/environment-detector.spec.em.renderer.ts
@@ -1,17 +1,17 @@
 import assert = require('assert');
 import { EnvironmentDetector } from '../../../src-shared/environment/environment-detector';
 
-describe('EnvironmentDetector (in main process in electron-mocha)', () => {
+describe('EnvironmentDetector (in renderer process in electron-mocha)', () => {
   it('isKarma should be false', () => {
     assert(EnvironmentDetector.isKarma === false);
   });
 
-  it('isElectronMochaMain should be true', () => {
-    assert(EnvironmentDetector.isElectronMochaMain === true);
+  it('isElectronMochaMain should be false', () => {
+    assert(EnvironmentDetector.isElectronMochaMain === false);
   });
 
-  it('isElectronMochaRenderer should be false', () => {
-    assert(EnvironmentDetector.isElectronMochaRenderer === false);
+  it('isElectronMochaRenderer should be true', () => {
+    assert(EnvironmentDetector.isElectronMochaRenderer === true);
   });
 
   it('isUnitTest should be true', () => {
