6d68671d3f3fbeffa94bf55ae6e1e7b9f7f722c9
Tomoyuki Aota <tomoyukia@gmail.com>
2019-05-22 03:00:34 +0900

    Remove SystemJS-related type declaration.

--- type-declaration/index.d.ts
+++ type-declaration/index.d.ts
@@ -1,9 +1,3 @@
-/* SystemJS module definition */
-declare var nodeModule: NodeModule;
-interface NodeModule {
-  id: string;
-}
-
 declare var window: Window;
 interface Window {
   process: NodeJS.Process;
