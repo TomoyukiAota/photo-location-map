483a10cbe62eaa6de63f78124a232f467619760c
Tomoyuki Aota <tomoyukia@gmail.com>
2019-05-22 02:53:08 +0900

    Split into multiple *.d.ts files.

--- type-declaration/exif-parser.d.ts
+++ type-declaration/exif-parser.d.ts
@@ -1,35 +1,5 @@
-/* SystemJS module definition */
-declare var nodeModule: NodeModule;
-interface NodeModule {
-  id: string;
-}
-
-interface NodeRequireFunction {
-  // Add overloads here in order to add return types of global.require and window.require functions.
-  (id: 'child_process'): typeof import('child_process');
-  (id: 'fs'): typeof import('fs');
-  (id: 'fs-extra'): typeof import('fs-extra');
-  (id: 'os'): typeof import('os');
-  (id: 'path'): typeof import('path');
-}
-
-declare namespace Electron {
-  interface Remote {
-    // Add overloads here in order to add return types of window.require('electron').remote.require function.
-    require(module: 'fs-extra'): typeof import('fs-extra');
-    require(module: 'os'): typeof import('os');
-  }
-}
-
-declare var window: Window;
-interface Window {
-  process: NodeJS.Process;
-  require: NodeRequire;
-}
-
-type DirectoryTree = ReturnType<typeof import('directory-tree')>;
-
 declare module 'exif-parser';
+
 interface ExifParserResult {
   tags?: {
     Make?: string,
@@ -85,7 +55,3 @@ interface ExifParserResult {
     LensModel?: string
   };
 }
-
-declare var __karma__: any;
-declare var __electronMochaMain__: any;
-declare var __electronMochaRenderer__: any;
