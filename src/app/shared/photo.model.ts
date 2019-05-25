191be812217ad92c247261ef33c470660b3ab99f
Tomoyuki Aota <tomoyukia@gmail.com>
2019-05-25 23:27:27 +0900

    Use GPS Info to determine tree node selectability.

--- src/app/shared/photo.model.ts
+++ src/app/shared/photo.model.ts
@@ -1,5 +1,11 @@
+export class GpsInfo {
+  gpsLatitude: number;
+  gpsLongitude: number;
+}
+
 export class Photo {
   name: string;
   path: string;
   exifParserResult: ExifParserResult;
+  gpsInfo: GpsInfo;
 }
