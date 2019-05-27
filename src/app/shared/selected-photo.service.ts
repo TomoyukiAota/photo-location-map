b910cda6ebcf01f3f48f02d236a6844150da2cf2
Tomoyuki Aota <tomoyukia@gmail.com>
2019-05-27 22:58:53 +0900

    Use PhotoDataService to get selected photos from selected paths.

--- src/app/shared/selected-photo.service.ts
+++ src/app/shared/selected-photo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Logger } from '../../../src-shared/log/logger';
 import { Photo } from './photo.model';
+import { PhotoDataService } from './photo-data.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,11 @@ import { Photo } from './photo.model';
 export class SelectedPhotoService {
   private selectedPhotos: Photo[];
 
+  constructor(private photoDataService: PhotoDataService) {
+  }
+
   public update(selectedPaths: string[]) {
-    const selectedPhotos = selectedPaths.map(path => {
-      // TODO: This path-photo mapping routine will be replaced with asking PhotoDataService with path which returns Photo.
-      const photo = new Photo();
-      photo.name = 'temp';
-      photo.path = path;
-      return photo;
-    });
+    const selectedPhotos = selectedPaths.map(path => this.photoDataService.getPhoto(path));
     Logger.info('Selected Photos: ', selectedPhotos);
     this.selectedPhotos = selectedPhotos;
   }
