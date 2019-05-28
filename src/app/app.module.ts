cc76dbf74eb12459628d26f0c12f32b9395653e8
Tomoyuki Aota <tomoyukia@gmail.com>
2019-05-29 03:18:44 +0900

    [NEED REVIST] Implement routine to fetch Google Maps API key from google-maps-api-key.json in AppData.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import '../polyfills';
 
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, Injectable } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule, MatTreeModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,7 +12,7 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AngularSplitModule } from 'angular-split';
-import { AgmCoreModule } from '@agm/core';
+import { AgmCoreModule, LAZY_MAPS_API_CONFIG, LazyMapsAPILoaderConfigLiteral } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,10 +21,26 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { DirectoryTreeViewComponent } from './directory-tree-view/directory-tree-view.component';
 import { MapComponent } from './map/map/map.component';
 import { GoogleMapsComponent } from './map/google-maps/google-maps.component';
+import { GoogleMapsApiKeyHandler } from './map/google-maps/google-maps-api-key-handler';
+import { Logger } from '../../src-shared/log/logger';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+const googleMapsApiKey = GoogleMapsApiKeyHandler.apiKey;
+Logger.info(`Google Maps API Key: ${googleMapsApiKey}`);
+
+@Injectable()
+export class MapsConfig implements LazyMapsAPILoaderConfigLiteral {
+  public apiKey: string;
+  public libraries: string[];
+  constructor() {
+    this.apiKey = GoogleMapsApiKeyHandler.apiKey;
+    this.libraries = ['places'];
+    console.log('lazy map init with ' + this.apiKey);
+  }
 }
 
 @NgModule({
@@ -56,9 +72,10 @@ export function HttpLoaderFactory(http: HttpClient) {
     MatIconModule,
     MatInputModule,
     MatTreeModule,
-    AgmCoreModule.forRoot({
-      apiKey: ''
-    })
+    AgmCoreModule.forRoot()
+  ],
+  providers: [
+    {provide: LAZY_MAPS_API_CONFIG, useClass: MapsConfig}
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/map/google-maps/google-maps-api-key-handler.ts
@@ -0,0 +1,33 @@
+const app = window.require('electron').remote.app;
+const path = window.require('path');
+// const fs = window.require('fs');
+
+export class GoogleMapsApiKeyHandler {
+  private static readonly fileName = 'google-maps-api-key.json';
+  private static filePath = path.join(window.require('electron').remote.app.getPath('userData'), GoogleMapsApiKeyHandler.fileName);
+  public static apiKey = GoogleMapsApiKeyHandler.fetchApiKey();
+
+  // public static initialize() {
+  //   this.filePath = path.join(window.require('electron').remote.app.getPath('userData'), GoogleMapsApiKeyHandler.fileName);
+  //   this.apiKey = this.fetchApiKey();
+  // }
+
+  public static fetchApiKey(): string {
+    let apiKey = '';
+
+    try {
+      const fs = window.require('fs');
+      const fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf8'));
+      // const jsonfile = window.require('electron').remote.require('jsonfile');
+
+      // const fileContent = jsonfile.readFileSync(this.filePath);
+      apiKey = fileContent.apiKey;
+    } catch (err) {
+      // TODO: Log error cases.
+    }
+
+    return apiKey;
+  }
+}
+
+// GoogleMapsApiKeyHandler.initialize();
