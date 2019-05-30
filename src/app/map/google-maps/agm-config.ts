53ef3413e7de68f0055e00c5df9c888fb9c6ad76
Tomoyuki Aota <tomoyukia@gmail.com>
2019-05-31 02:31:43 +0900

    Use GoogleMapsApiKeyHandler.fetchApiKey()

--- src/app/map/google-maps/agm-config.ts
+++ src/app/map/google-maps/agm-config.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { LazyMapsAPILoaderConfigLiteral } from '@agm/core';
+import { Logger } from '../../../../src-shared/log/logger';
 import { GoogleMapsApiKeyHandler } from './google-maps-api-key-handler';
 
 @Injectable()
 export class AgmConfig implements LazyMapsAPILoaderConfigLiteral {
   public apiKey: string;
   constructor() {
-    this.apiKey = GoogleMapsApiKeyHandler.apiKey;
-    console.log('lazy map init with ' + this.apiKey);
+    this.apiKey = GoogleMapsApiKeyHandler.fetchApiKey();
+    Logger.info(`Google Maps API Key: ${this.apiKey}`);
   }
 }
