4aa0e4b32f460d53b869760b9d594ef86b98ab77
Tomoyuki Aota <tomoyukia@gmail.com>
2019-05-31 03:24:36 +0900

    Improve log messages. Handle the case when google-maps-api-key.json does not exist.

--- src/app/map/google-maps/agm-config.ts
+++ src/app/map/google-maps/agm-config.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { LazyMapsAPILoaderConfigLiteral } from '@agm/core';
-import { Logger } from '../../../../src-shared/log/logger';
 import { GoogleMapsApiKeyHandler } from './google-maps-api-key-handler';
 
 @Injectable()
 export class AgmConfig implements LazyMapsAPILoaderConfigLiteral {
   public apiKey: string;
+
   constructor() {
     this.apiKey = GoogleMapsApiKeyHandler.fetchApiKey();
-    Logger.info(`Google Maps API Key: ${this.apiKey}`);
   }
 }
--- src/app/map/google-maps/google-maps-api-key-handler.ts
+++ src/app/map/google-maps/google-maps-api-key-handler.ts
@@ -1,3 +1,5 @@
+import { Logger } from '../../../../src-shared/log/logger';
+
 const app = window.require('electron').remote.app;
 const fs = window.require('fs');
 const path = window.require('path');
@@ -5,24 +7,40 @@ const path = window.require('path');
 export class GoogleMapsApiKeyHandler {
   private static readonly fileName = 'google-maps-api-key.json';
   private static filePath: string;
-  private static apiKey: string;
 
   public static initialize() {
     this.filePath = path.join(app.getPath('userData'), GoogleMapsApiKeyHandler.fileName);
-    this.apiKey = this.fetchApiKey();
   }
 
   public static fetchApiKey(): string {
+    if (!fs.existsSync(this.filePath)) {
+      Logger.info(`The file for Google Maps API Key does not exist.`);
+      this.logHowToUseGoogleMapsMessage();
+      return '';
+    }
+
+    return this.fetchApiKeyFromFile();
+  }
+
+  private static fetchApiKeyFromFile(): string {
     let apiKey = '';
 
     try {
-      const fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf8'));
-      apiKey = fileContent.apiKey;
+      const fileContent = fs.readFileSync(this.filePath, 'utf8');
+      const jsonObject = JSON.parse(fileContent);
+      apiKey = jsonObject.apiKey;
+      Logger.info(`Fetched Google Maps API Key "${apiKey}" from "${this.filePath}".`);
     } catch (err) {
-      // TODO: Log error cases.
+      Logger.warn(`Failed to fetch Google Maps API Key from "${this.filePath}".`);
+      this.logHowToUseGoogleMapsMessage();
     }
 
     return apiKey;
+  }
+
+  private static logHowToUseGoogleMapsMessage() {
+    Logger.info(`The Google Maps API Key will be '' (empty string).`);
+    Logger.info(`To use Google Maps, create "${this.filePath}" with file content "{ "apiKey": "YOUR_API_KEY" }"`);
   }
 }
 
