cbc17de3379b9bd7b16eb5ea6eb40cf882dc8be6
Tomoyuki Aota <tomoyukia@gmail.com>
2019-06-02 20:37:57 +0900

    Update parameterized tests.

--- src/app/shared/model/lat-lng.model.spec.ts
+++ src/app/shared/model/lat-lng.model.spec.ts
@@ -1,7 +1,7 @@
 import { calculateCenterLatLng, LatLng } from './lat-lng.model';
 
 describe('LatLng', () => {
-  it('should create', () => {
+  it('should contain latitude and longitude', () => {
     // Arrange
     const latitude = 10;
     const longitude = 20;
@@ -14,60 +14,71 @@ describe('LatLng', () => {
     expect(latLng.longitude).toEqual(longitude);
   });
 
-  it('add should return added latitude and longitude', () => {
-    // Arrange
-    const latLng1 = new LatLng(10, 11);
-    const latLng2 = new LatLng(20, 21);
-
-    // Act
-    const addedLatLng = latLng1.add(latLng2);
-
-    // Assert
-    expect(addedLatLng.latitude).toEqual(latLng1.latitude + latLng2.latitude);
-    expect(addedLatLng.longitude).toEqual(latLng1.longitude + latLng2.longitude);
-  });
-
-  it('add should return null when null or undefined is passed', () => {
-    [
-      null,
-      undefined
-    ].forEach(inputValue => {
+  describe('add', () => {
+    it('should return added latitude and longitude', () => {
       // Arrange
       const latLng1 = new LatLng(10, 11);
-      const latLng2 = inputValue;
+      const latLng2 = new LatLng(20, 21);
 
       // Act
       const addedLatLng = latLng1.add(latLng2);
 
       // Assert
-      expect(addedLatLng).toBeNull();
+      expect(addedLatLng.latitude).toEqual(latLng1.latitude + latLng2.latitude);
+      expect(addedLatLng.longitude).toEqual(latLng1.longitude + latLng2.longitude);
+    });
+
+    describe('should return null', () => {
+      const testCases = [
+        {description: 'when null is passed'          , inputValue: null     },
+        {description: 'when undefined is passed'     , inputValue: undefined}
+      ];
+
+      testCases.forEach(({description, inputValue}) => {
+        it(description, () => {
+          // Arrange
+          const latLng = new LatLng(10, 11);
+
+          // Act
+          const addedLatLng = latLng.add(inputValue);
+
+          // Assert
+          expect(addedLatLng).toBeNull();
+        });
+      });
     });
   });
 
-  it('calculateCenterLatLng should return center latitude and longitude', () => {
-    // Arrange
-    const latLng1 = new LatLng(10, 12);
-    const latLng2 = new LatLng(20, 22);
-    const totalLatLng = latLng1.add(latLng2);
-    const expectedCenterLat = totalLatLng.latitude / 2;
-    const expectedCenterLng = totalLatLng.longitude / 2;
+  describe('calculateCenterLatLng', () => {
+    it('should return center latitude and longitude', () => {
+      // Arrange
+      const latLng1 = new LatLng(10, 12);
+      const latLng2 = new LatLng(20, 22);
+      const totalLatLng = latLng1.add(latLng2);
+      const expectedCenterLat = totalLatLng.latitude / 2;
+      const expectedCenterLng = totalLatLng.longitude / 2;
 
-    // Act
-    const actualCenterLatLng = calculateCenterLatLng([latLng1, latLng2]);
+      // Act
+      const actualCenterLatLng = calculateCenterLatLng([latLng1, latLng2]);
 
-    // Assert
-    expect(actualCenterLatLng.latitude).toEqual(expectedCenterLat);
-    expect(actualCenterLatLng.longitude).toEqual(expectedCenterLng);
-  });
+      // Assert
+      expect(actualCenterLatLng.latitude).toEqual(expectedCenterLat);
+      expect(actualCenterLatLng.longitude).toEqual(expectedCenterLng);
+    });
 
-  it('calculateCenterLatLng should return null when null, undefined, or an empty array is passed', () => {
-    [
-      null,
-      undefined,
-      []
-    ].forEach(inputValue => {
-      const addedLatLng = calculateCenterLatLng(inputValue);
-      expect(addedLatLng).toBeNull();
+    describe('should return null', () => {
+      const testCases = [
+        {description: 'when null is passed'          , inputValue: null     },
+        {description: 'when undefined is passed'     , inputValue: undefined},
+        {description: 'when an empty array is passed', inputValue: []       }
+      ];
+
+      testCases.forEach(({description, inputValue}) => {
+        it(description, () => {
+          const addedLatLng = calculateCenterLatLng(inputValue);
+          expect(addedLatLng).toBeNull();
+        });
+      });
     });
   });
 });
