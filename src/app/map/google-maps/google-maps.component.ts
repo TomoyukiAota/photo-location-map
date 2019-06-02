e8cde8905b6d534b65991be8e552847bef32dfa9
Tomoyuki Aota <tomoyukia@gmail.com>
2019-06-02 23:14:27 +0900

    Display Google Maps using raw APIs (without Angular Google Maps).

--- src/app/map/google-maps/google-maps.component.ts
+++ src/app/map/google-maps/google-maps.component.ts
@@ -1,15 +1,17 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { SelectedPhotoService } from '../../shared/service/selected-photo.service';
 import { calculateCenterLatLng, LatLng } from '../../shared/model/lat-lng.model';
 import { Photo } from '../../shared/model/photo.model';
+
+declare var google;
 
 @Component({
   selector: 'app-google-maps',
   templateUrl: './google-maps.component.html',
   styleUrls: ['./google-maps.component.scss']
 })
-export class GoogleMapsComponent implements OnInit, OnDestroy {
+export class GoogleMapsComponent implements OnInit, OnDestroy, AfterViewInit {
   private subscription: Subscription;
   public photos: Photo[];
 
@@ -24,7 +26,9 @@ export class GoogleMapsComponent implements OnInit, OnDestroy {
     this.subscription = this.selectedPhotoService.selectedPhotosChanged.subscribe(
       (photos: Photo[]) => {
         this.photos = photos;
-        this.updateCenterLatLng(photos);
+        // this.updateCenterLatLng(photos);
+
+        console.log(window['google']);
         this.changeDetectorRef.detectChanges();
       }
     );
@@ -34,13 +38,46 @@ export class GoogleMapsComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  private updateCenterLatLng(photos: Photo[]) {
-    if (photos.length === 0)
-      return;
+  ngAfterViewInit(): void {
+    const script = document.createElement('script');
+    script.setAttribute('src', 'http://maps.google.com/maps/api/js');
+    script.setAttribute('type', 'text/javascript');
+    script.async = false;
 
-    const latLngArray = photos.map(photo => photo.gpsInfo.latLng);
-    const centerLatLng = calculateCenterLatLng(latLngArray);
-    this.centerLatitude = centerLatLng.latitude;
-    this.centerLongitude = centerLatLng.longitude;
+    const div = document.getElementById('google-map');
+    div.parentNode.insertBefore(script, null);
+
+    // // tslint:disable-next-line:no-unused-expression
+    // new google.maps.Map(document.getElementById('google-map'), {
+    //   mapTypeId: google.maps.MapTypeId.ROADMAP,
+    //   center: { lat: 0.0, lng: 0.0 },
+    //   zoom: 2
+    // });
+
+    // Wait for 1 second for Google Maps API script to load.
+    // Then, render initial state of Google Maps.
+    setTimeout(() => this.renderInitialGoogleMapsState(), 1000);
   }
+
+  private renderInitialGoogleMapsState(): void {
+    // "new" for google.maps.Map is required although the reference to the instantiated object is not required.
+    // Without "new", the map is not rendered.
+
+    // tslint:disable-next-line:no-unused-expression
+    new google.maps.Map(document.getElementById('google-map'), {
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      center: { lat: 0.0, lng: 0.0 },
+      zoom: 2
+    });
+  }
+
+  // private updateCenterLatLng(photos: Photo[]) {
+  //   if (photos.length === 0)
+  //     return;
+  //
+  //   const latLngArray = photos.map(photo => photo.gpsInfo.latLng);
+  //   const centerLatLng = calculateCenterLatLng(latLngArray);
+  //   this.centerLatitude = centerLatLng.latitude;
+  //   this.centerLongitude = centerLatLng.longitude;
+  // }
 }
