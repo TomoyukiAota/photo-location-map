99d6d4b3f16cb023747df0ed07517638a84bfc80
Tomoyuki Aota <tomoyukia@gmail.com>
2019-06-06 00:08:06 +0900

    Add error handling for loading Google Maps.

--- src/app/map/google-maps/google-maps-api-loader.ts
+++ src/app/map/google-maps/google-maps-api-loader.ts
@@ -1,18 +1,32 @@
 import { Logger } from '../../../../src-shared/log/logger';
 
 export class GoogleMapsApiLoader {
+  // If Google Maps API cannot be loaded in 20 seconds, something should went wrong.
+  private static readonly maxDuration = 20000; // 20 seconds
+
+  private static startTime: number;
+  private static intervalId: NodeJS.Timeout;
+
   public static waitUntilLoaded(): Promise<void> {
-    return new Promise((resolve) => {
-      const startTime = performance.now();
-      const id = setInterval(() => {
-        if (typeof google !== 'undefined') {
-          clearInterval(id);
-          const endTime = performance.now();
-          const duration = endTime - startTime;
-          Logger.info(`It took ${duration} ms to load Google Maps API.`);
-          resolve();
-        }
-      }, 1);
+    this.startTime = performance.now();
+    return new Promise((resolve, reject) => {
+      this.intervalId = setInterval(() => this.checkIfGoogleMapsApiIsLoaded(resolve, reject), 1);
     });
+  }
+
+  private static checkIfGoogleMapsApiIsLoaded(resolve: (value?: void | PromiseLike<void>) => void,
+                                              reject: (reason?: any) => void): void {
+    const currentTime = performance.now();
+    const currentDuration = currentTime - this.startTime;
+    const isApiLoaded = typeof google !== 'undefined';
+    if (isApiLoaded) {
+      clearInterval(this.intervalId);
+      Logger.info(`It took ${currentDuration} ms to load Google Maps API.`);
+      resolve();
+    } else if (currentDuration > this.maxDuration) {
+      clearInterval(this.intervalId);
+      Logger.error(`Waiting Google Maps API for more than ${this.maxDuration} ms, but it is still not loaded.`);
+      reject();
+    }
   }
 }
