f48017d68c231226c323bc07de55ff3b2693f8fe
Tomoyuki Aota <tomoyukia@gmail.com>
2019-06-10 20:26:26 +0900

    Display thumbnail on info window.

--- src/app/shared/model/photo.model.ts
+++ src/app/shared/model/photo.model.ts
@@ -1,9 +1,11 @@
 import { GpsInfo } from './gps-info.model';
+import { Thumbnail } from './thumbnail.model';
 
 export class Photo {
   name: string;
   path: string;
   exifParserResult: ExifParserResult;
   gpsInfo: GpsInfo;
+  thumbnail: Thumbnail;
   dateTimeTaken: string;
 }
