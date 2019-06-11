11e73d782a8c76b37378bceaf8965ad449a70c0f
Tomoyuki Aota <tomoyukia@gmail.com>
2019-06-12 03:27:24 +0900

    Launch photo viewer when thunbnail is clicked (work in progress).

---
+++ src/app/shared/service/photo-viewer-launcher.ts
@@ -0,0 +1,46 @@
+import { Photo } from '../model/photo.model';
+const BrowserWindow = window.require('electron').remote.BrowserWindow;
+
+export class PhotoViewerLauncher {
+  public static launch(photo: Photo): void {
+    console.log('launched!!!!');
+    // const windowRef = window.open('', photo.name, 'width=600,height=600');
+
+    let browserWindow = new BrowserWindow({
+      title: photo.name,
+      width: 600,
+      height: 600,
+      useContentSize: true,
+      webPreferences: {
+        webSecurity: false
+      }
+    });
+    browserWindow.on('close', function () { browserWindow = null; });
+
+    // browserWindow.setTitle(photo.name);
+
+    const loadView = (p: Photo) => {
+      return (`
+        <!DOCTYPE html>
+        <html lang="en-us">
+          <head>
+            <title>${p.name}</title>
+            <meta charset="UTF-8">
+          </head>
+          <body>
+            <img src="${p.path}" width="600" height="600">
+            <script>
+              console.log("logged.");
+            </script>
+          </body>
+        </html>
+      `);
+    };
+
+    const file = 'data:text/html;charset=UTF-8,' + encodeURIComponent(loadView(photo));
+
+    browserWindow.loadURL(file);
+    // browserWindow.setMenu(null);
+    browserWindow.show();
+  }
+}
