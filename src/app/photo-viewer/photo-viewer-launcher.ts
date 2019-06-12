ba352a6d13b3e5f93d2a3066a6622220db7bb035
Tomoyuki Aota <tomoyuki.aota@keysight.com>
2019-06-12 19:53:17 +0900

    Display photo considering width and height.

--- src/app/photo-viewer/photo-viewer-launcher.ts
+++ src/app/photo-viewer/photo-viewer-launcher.ts
@@ -4,43 +4,64 @@ const BrowserWindow = window.require('electron').remote.BrowserWindow;
 export class PhotoViewerLauncher {
   public static launch(photo: Photo): void {
     console.log('launched!!!!');
-    // const windowRef = window.open('', photo.name, 'width=600,height=600');
+
+    const photoDisplaySize = this.shrinkSizeAccordingToScreen(photo.width, photo.height);
 
     let browserWindow = new BrowserWindow({
       title: photo.name,
-      width: 600,
-      height: 600,
+      width: photoDisplaySize.width,
+      height: photoDisplaySize.height,
       useContentSize: true,
       webPreferences: {
         webSecurity: false
       }
     });
+
     browserWindow.on('close', function () { browserWindow = null; });
 
-    // browserWindow.setTitle(photo.name);
+    const dataUrl = 'data:text/html;charset=UTF-8,' + encodeURIComponent(this.createHtmlString(photo, photoDisplaySize));
+    browserWindow.loadURL(dataUrl);
+    // browserWindow.setMenu(null);
+    browserWindow.show();
+  }
 
-    const loadView = (p: Photo) => {
-      return (`
+  private static shrinkSizeAccordingToScreen(srcWidth: number, srcHeight: number) {
+    const maxSrcToScreenRatio = 0.9;
+    const srcToScreenWidthRatio = srcWidth / window.screen.width;
+    const srcToScreenHeightRatio = srcHeight / window.screen.height;
+    const srcToScreenLargerSideRatio = Math.max(srcToScreenWidthRatio, srcToScreenHeightRatio);
+
+    if (srcToScreenLargerSideRatio <= maxSrcToScreenRatio) {
+      return {
+        width: srcWidth,
+        height: srcHeight
+      };
+    }
+
+    const shrinkRatio = maxSrcToScreenRatio / srcToScreenLargerSideRatio;
+    const shrinkedWidth  = srcWidth  * shrinkRatio;
+    const shrinkedHeight = srcHeight * shrinkRatio;
+    return {
+      width: shrinkedWidth,
+      height: shrinkedHeight
+    };
+  }
+
+  private static createHtmlString(photo: Photo, photoDisplaySize: { width: number, height: number }) {
+    return `
         <!DOCTYPE html>
         <html lang="en-us">
           <head>
-            <title>${p.name}</title>
+            <title>${photo.name}</title>
             <meta charset="UTF-8">
           </head>
           <body>
-            <img src="${p.path}" width="600" height="600">
+            <img src="${photo.path}" width="${photoDisplaySize.width}" height="${photoDisplaySize.height}">
             <script>
               console.log("logged.");
             </script>
           </body>
         </html>
-      `);
-    };
-
-    const file = 'data:text/html;charset=UTF-8,' + encodeURIComponent(loadView(photo));
-
-    browserWindow.loadURL(file);
-    // browserWindow.setMenu(null);
-    browserWindow.show();
+    `;
   }
 }
