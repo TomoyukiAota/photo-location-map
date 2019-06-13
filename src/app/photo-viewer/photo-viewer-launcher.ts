9cc8fb40eca083cb1d7e7a890375d712330bd4ac
Tomoyuki Aota <tomoyukia@gmail.com>
2019-06-14 03:51:32 +0900

    Display Rotate button when hovering mouse on photo.

--- src/app/photo-viewer/photo-viewer-launcher.ts
+++ src/app/photo-viewer/photo-viewer-launcher.ts
@@ -74,10 +74,38 @@ export class PhotoViewerLauncher {
                 object-fit: contain;
                 image-orientation: from-image;
               }
+
+              button {
+                position: absolute;
+                top: 90%;
+                left: 90%;
+                transform: translate(-50%, -50%);
+                background-color: #555;
+                color: white;
+                font-size: 16px;
+                padding: 12px 24px;
+                border: none;
+                cursor: pointer;
+                border-radius: 5px;
+                text-align: center;
+                opacity: 0;
+                transition: opacity .35s ease;
+              }
+
+              .container:hover button {
+                opacity: 1;
+              }
+
+              button:hover {
+                background-color: #303030;
+              }
             </style>
           </head>
           <body>
-            <img src="${photo.path}" width="${viewerDimensions.width}" height="${viewerDimensions.height}" alt="${photo.name}">
+            <div class="container">
+              <img src="${photo.path}" width="${viewerDimensions.width}" height="${viewerDimensions.height}" alt="${photo.name}">
+              <button>Rotate</button>
+            </div>
             <script>
               console.info('This is the photo viewer for ' + '${escapedPhotoPath}');
             </script>
