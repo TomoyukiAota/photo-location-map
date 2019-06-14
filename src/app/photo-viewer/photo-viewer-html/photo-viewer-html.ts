87ecd8243db298caef1a52e5d308b5b413eafb8e
Tomoyuki Aota <tomoyukia@gmail.com>
2019-06-14 22:04:02 +0900

    Add photo-viewer-script-tag.ts

--- src/app/photo-viewer/photo-viewer-html/photo-viewer-html.ts
+++ src/app/photo-viewer/photo-viewer-html/photo-viewer-html.ts
@@ -1,11 +1,10 @@
 import { Photo } from '../../shared/model/photo.model';
 import { Dimensions } from '../../shared/model/dimensions.model';
 import { PhotoViewerStyleTag } from './photo-viewer-style-tag';
+import { PhotoViewerScriptTag } from './photo-viewer-script-tag';
 
 export class PhotoViewerHtml {
   public static create(photo: Photo, viewerDimensions: Dimensions): string {
-    const escapedPhotoPath = photo.path.replace(/\\/g, '\\\\');
-
     return `
 <!DOCTYPE html>
 <html lang="en-us">
@@ -19,9 +18,7 @@ export class PhotoViewerHtml {
       <img src="${photo.path}" width="${viewerDimensions.width}" height="${viewerDimensions.height}" alt="${photo.name}">
       <button>Rotate</button>
     </div>
-    <script>
-      console.info('This is the photo viewer for ' + '${escapedPhotoPath}');
-    </script>
+    ${PhotoViewerScriptTag.create(photo, viewerDimensions)}
   </body>
 </html>
     `;
