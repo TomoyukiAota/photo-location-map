96fbaf1031ab72a1cb20eb3dc962e2acbb7ae560
Tomoyuki Aota <tomoyukia@gmail.com>
2019-06-15 23:55:11 +0900

    Launch associated application on Linux.

--- src/app/photo-viewer/photo-viewer-launcher.ts
+++ src/app/photo-viewer/photo-viewer-launcher.ts
@@ -5,29 +5,32 @@ import { FallbackPhotoViewer } from './fallback-photo-viewer/fallback-photo-view
 const child_process = window.require('child_process');
 const os = window.require('os');
 
-const commandExistsSync = window.require('command-exists').sync;
-
 export class PhotoViewerLauncher {
   public static launch(photo: Photo): void {
-    if (os.platform() === 'win32') {
-      this.launchAssociatedAppOnWindows(photo);
-    } else if (commandExistsSync('open')) {
-      // On non-Windows platform, if "open" command is available, use it to launch the associated application.
-      // At least on macOS and Ubuntu, "open" command is available.
-      this.launchAssociatedAppOnNonWindows(photo);
+    const command = this.getCommandToLaunchAssociatedApp(photo);
+    if (command) {
+      this.launchAssociatedApp(command, photo);
     } else {
-      // On non-Windows platform, if "open" command is not available, launch the fallback photo viewer.
       this.launchFallbackPhotoViewer(photo);
     }
   }
 
-  private static launchAssociatedAppOnWindows(photo: Photo): void {
-    child_process.spawn(`explorer "${photo.path}"`, [], { shell: true });
-    Logger.info(`Launched the associated application for ${photo.path}`, photo);
+  private static getCommandToLaunchAssociatedApp(photo: Photo): string {
+    switch (os.platform()) {
+      case 'win32':
+        return `explorer "${photo.path}"`;
+      case 'darwin':
+        return `open "${photo.path}"`;
+      case 'linux':
+        return `xdg-open "${photo.path}"`
+      default:
+        return null;
+    }
   }
 
-  private static launchAssociatedAppOnNonWindows(photo: Photo): void {
-    child_process.spawn(`open "${photo.path}"`, [], {shell: true});
+  private static launchAssociatedApp(command: string, photo: Photo): void {
+    child_process.spawn(command, [], { shell: true });
+    Logger.info(`Issued a command: ${command}`);
     Logger.info(`Launched the associated application for ${photo.path}`, photo);
   }
 
