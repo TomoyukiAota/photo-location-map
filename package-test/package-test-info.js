3cf811d9aeea34998fe3772af050144bdaa5acf0
Tomoyuki Aota <tomoyukia@gmail.com>
2019-06-28 01:24:45 +0900

    Update productName.

--- package-test/package-test-info.js
+++ package-test/package-test-info.js
@@ -12,18 +12,18 @@ class PackageTestInfo {
     switch(global.process.platform) {
       case 'win32':
         this.packageCreationCommand = 'npm run electron:windows';
-        this.expectedPackageLocation = `${this.releaseDirectory}\\angular-electron 0.0.1.exe`;
-        this.executableLaunchCommand = `"${this.releaseDirectory}\\angular-electron 0.0.1.exe"`;
+        this.expectedPackageLocation = `${this.releaseDirectory}\\Photo Location Map 0.0.1.exe`;
+        this.executableLaunchCommand = `"${this.releaseDirectory}\\Photo Location Map 0.0.1.exe"`;
         break;
       case 'darwin':
         this.packageCreationCommand = 'npm run electron:mac';
-        this.expectedPackageLocation = `${this.releaseDirectory}/angular-electron-0.0.1.dmg`;
-        this.executableLaunchCommand = `hdiutil attach ${this.releaseDirectory}/angular-electron-0.0.1.dmg && open -W "/Volumes/angular-electron 0.0.1/angular-electron.app"`;
+        this.expectedPackageLocation = `${this.releaseDirectory}/Photo Location Map-0.0.1.dmg`;
+        this.executableLaunchCommand = `hdiutil attach "${this.releaseDirectory}/Photo Location Map-0.0.1.dmg" && open -W "/Volumes/Photo Location Map 0.0.1/Photo Location Map.app"`;
         break;
       case 'linux':
         this.packageCreationCommand = 'npm run electron:linux';
-        this.expectedPackageLocation = `${this.releaseDirectory}/angular-electron 0.0.1.AppImage`;
-        this.executableLaunchCommand = `"${this.releaseDirectory}/angular-electron 0.0.1.AppImage"`;
+        this.expectedPackageLocation = `${this.releaseDirectory}/Photo Location Map 0.0.1.AppImage`;
+        this.executableLaunchCommand = `"${this.releaseDirectory}/Photo Location Map 0.0.1.AppImage"`;
         break;
       default:
         throw new Error(`Unsupported platform for "${__filename}"`);
