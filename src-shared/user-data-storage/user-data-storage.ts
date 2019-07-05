7d98e3fc656a2521c9d38f6f70c2044b0330c7e6
Tomoyuki Aota <tomoyukia@gmail.com>
2019-07-06 00:27:26 +0900

    Introduce UserDataStorage.

---
+++ src-shared/user-data-storage/user-data-storage.ts
@@ -0,0 +1,39 @@
+import { RequireFromMainProcess } from '../require/require-from-main-process';
+
+export class UserDataStorage {
+  private static fsExtra = RequireFromMainProcess.fsExtra;
+  private static path = RequireFromMainProcess.path;
+  private static userDataPath = RequireFromMainProcess.electron.app.getPath('userData');
+  private static storageRootPath = UserDataStorage.path.join(UserDataStorage.userDataPath, 'PhotoLocationMapStorage');
+
+  public static read(storagePath: string[]): string {
+    const {filePath, key} = this.getFilePathAndKey(storagePath);
+
+    if (!this.fsExtra.existsSync(filePath))
+      throw new Error(`File for ${key} does not exist in ${filePath}`);
+
+    const fileContent = this.fsExtra.readFileSync(filePath, 'utf8');
+    const jsonObject = JSON.parse(fileContent);
+    const value = jsonObject[key];
+    return value;
+  }
+
+  public static write(storagePath: string[], value: string): void {
+    const {filePath, key} = this.getFilePathAndKey(storagePath);
+    const jsonObject = {};
+    jsonObject[key] = value;
+    const fileContent = JSON.stringify(jsonObject);
+    this.fsExtra.ensureFileSync(filePath);
+    this.fsExtra.writeFileSync(filePath, fileContent);
+  }
+
+  private static getFilePathAndKey(storagePath: string[]): {filePath: string, key: string} {
+    if (!storagePath || !storagePath.length) {
+      throw new Error('storagePath needs to be a string array which contains at least 1 element.');
+    }
+
+    const filePath = this.path.join(this.storageRootPath, ...storagePath);
+    const key = storagePath[storagePath.length - 1];
+    return {filePath, key};
+  }
+}
---
+++ src-shared/user-data-storage/user-data-stroage-path.ts
@@ -0,0 +1,5 @@
+export namespace UserDataStoragePath {
+  export namespace Analytics {
+    export const UserId = ['Analytics', 'UserId'];
+  }
+}
