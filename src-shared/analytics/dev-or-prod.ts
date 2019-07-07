f5f97dcbfc6964b0a76d7fa60e999e3a4ba49ac8
Tomoyuki Aota <tomoyukia@gmail.com>
2019-07-07 23:40:45 +0900

    Use different Google Analytics Tracking ID for development and production.

--- src-shared/analytics/dev-or-prod.ts
+++ src-shared/analytics/dev-or-prod.ts
@@ -1,1 +1,27 @@
 export type DevOrProd = 'Dev' | 'Prod';
+
+const fetchDevOrProdFromConfig: (() => DevOrProd) = () => {
+  // devOrProdFromConfig is set from "config" property in package.json
+  // Also, package.json does not exist in the packaged application.
+  // This means the followings:
+  //  - If npm script launches non-packaged application (e.g. npm start), devOrProdFromConfig is set.
+  //  - If npm script launches packaged application (i.e. package-test), devOrProdFromConfig is set.
+  //  - If packaged application is directly launched (i.e. publicly released application), devOrProdFromConfig is not set (undefined).
+  const devOrProdFromConfig = process.env.npm_package_config_devOrProd as DevOrProd;
+
+  if (devOrProdFromConfig === 'Dev') {
+    return 'Dev';
+  } else if (devOrProdFromConfig === 'Prod') {
+    return  'Prod';
+  } else if (!devOrProdFromConfig) {
+    return 'Prod';
+  } else {
+    throw new Error(`Either "Dev" or "Prod" needs to be specified "config" property in package.json. Currently, it is "${devOrProdFromConfig}".`);
+  }
+};
+
+const devOrProd = fetchDevOrProdFromConfig();
+
+export const getDevOrProd: (() => DevOrProd) = () => {
+  return devOrProd;
+};
