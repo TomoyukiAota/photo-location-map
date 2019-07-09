2aaff120fb4c2f646e887b074f0d36b8b5f31cc1
Tomoyuki Aota <tomoyukia@gmail.com>
2019-07-10 02:02:57 +0900

    Record app launch date, last launch date, first launch date, period of use, and launch count.

--- src-shared/user-data-storage/user-data-stroage-path.ts
+++ src-shared/user-data-storage/user-data-stroage-path.ts
@@ -3,6 +3,12 @@ export namespace UserDataStoragePath {
     export const UserId: ReadonlyArray<string> = ['Analytics', 'UserId'];
   }
 
+  export namespace History {
+    export const FirstLaunchDateTime: ReadonlyArray<string> = ['History', 'FirstLaunchDateTime'];
+    export const LastLaunchDateTime: ReadonlyArray<string> = ['History', 'LastLaunchDateTime'];
+    export const LaunchCount: ReadonlyArray<string> = ['History', 'LaunchCount'];
+  }
+
   export namespace GoogleMaps {
     export const ApiKey: ReadonlyArray<string> = ['GoogleMaps', 'ApiKey'];
   }
