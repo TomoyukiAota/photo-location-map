87148f52d94fbdf4268af44be6d4ef09f020d985
Tomoyuki Aota <tomoyukia@gmail.com>
2019-07-09 23:06:46 +0900

    Add comments about date time formats.

--- src-shared/date-time/date-time-format.ts
+++ src-shared/date-time/date-time-format.ts
@@ -1,4 +1,4 @@
 export class DateTimeFormat {
-  public static basic = 'YYYYMMDDTHHmmss.SSS[Z]';
-  public static extended = 'YYYY-MM-DDTHH:mm:ss.SSS[Z]';
+  public static basic    = 'YYYYMMDDTHHmmss.SSS[Z]';      // Not very human-readable format, but this can be used in path.
+  public static extended = 'YYYY-MM-DDTHH:mm:ss.SSS[Z]';  // Human-readable format
 }
