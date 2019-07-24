e9afee6ccca7d3ae03aefcf9eacf502ecddcadd5
Tomoyuki Aota <tomoyukia@gmail.com>
2019-07-25 02:21:53 +0900

    Record numbers of total items, directories, files, JPEG files, and Live Photos from selected folder.

--- src/app/shared/supported-filename-extensions.ts
+++ src/app/shared/supported-filename-extensions.ts
@@ -1,9 +1,13 @@
 export class SupportedFilenameExtensions {
-  public static readonly jpegExtensions = ['.jpeg', '.jpg', '.jpe', '.jfif', '.jfi', '.jif'];
+  public static readonly jpegExtensions: ReadonlyArray<string> = ['.jpeg', '.jpg', '.jpe', '.jfif', '.jfi', '.jif'];
   public static readonly supportedExtensions = SupportedFilenameExtensions.jpegExtensions;
 
   public static isSupported(extension: string) {
     const isSupportedExtension = this.supportedExtensions.includes(extension);
     return isSupportedExtension;
+  }
+
+  public static isJpeg(extension: string) {
+    return this.jpegExtensions.includes(extension);
   }
 }
