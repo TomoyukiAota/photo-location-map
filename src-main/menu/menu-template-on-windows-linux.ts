770fa13aaf10a2f08fa63e6ad539ddb3922cef4a
Tomoyuki Aota <tomoyukia@gmail.com>
2019-08-15 20:49:21 +0900

    Introduce Help menu.

--- src-main/menu/menu-template-on-windows-linux.ts
+++ src-main/menu/menu-template-on-windows-linux.ts
@@ -2,5 +2,19 @@ import { dialog, MenuItemConstructorOptions } from 'electron';
 import { commonMenuTemplate } from './common-menu-template';
 
 export const menuTemplateOnWindowsLinux: MenuItemConstructorOptions[] = [
-  ...commonMenuTemplate
+  ...commonMenuTemplate,
+  {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'Welcome'
+      },
+      { type: 'separator' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      {
+        label: 'Privacy Statement'
+      }
+    ]
+  }
 ];
