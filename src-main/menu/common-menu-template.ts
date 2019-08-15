9d74d4c997d9caaa812a0201614b476b300c4877
Tomoyuki Aota <tomoyukia@gmail.com>
2019-08-15 20:27:19 +0900

    Introduce common-menu-template.ts

---
+++ src-main/menu/common-menu-template.ts
@@ -0,0 +1,73 @@
+import { dialog, MenuItemConstructorOptions } from 'electron';
+
+export const commonMenuTemplate: MenuItemConstructorOptions[] = [
+  {
+    label: 'Selection',
+    submenu: [
+      {
+        label: 'Undo',
+        click(item, focusedWindow) {
+          const options = {
+            type: 'info',
+            buttons: ['OK'],
+            title: 'Undo Menu Clicked',
+            message: 'Undo menu is clicked!',
+            detail: 'This will be replaced with undo selection of item(s) in treeview.'
+          };
+          dialog.showMessageBox(focusedWindow, options);
+        }
+      },
+      {
+        label: 'Redo',
+        click(item, focusedWindow) {
+          const options = {
+            type: 'info',
+            buttons: ['OK'],
+            title: 'Redo Menu Clicked',
+            message: 'Redo menu is clicked!',
+            detail: 'This will be replaced with redo selection of item(s) in treeview.'
+          };
+          dialog.showMessageBox(focusedWindow, options);
+        }
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click(item, focusedWindow) {
+          if (focusedWindow) focusedWindow.reload();
+        },
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'resetzoom'
+      },
+      {
+        role: 'zoomin'
+      },
+      {
+        role: 'zoomout'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'togglefullscreen'
+      }
+    ]
+  },
+  {
+    label: 'Tools',
+    submenu: [
+      {
+        role: 'toggledevtools'
+      }
+    ]
+  }
+];
--- src-main/menu/menu-template-on-mac.ts
+++ src-main/menu/menu-template-on-mac.ts
@@ -1,4 +1,5 @@
 import { dialog, MenuItemConstructorOptions } from 'electron';
+import { commonMenuTemplate } from './common-menu-template';
 
 export const menuTemplateOnMac: MenuItemConstructorOptions[] = [
   {
@@ -15,73 +16,5 @@ export const menuTemplateOnMac: MenuItemConstructorOptions[] = [
       { role: 'quit' }
     ]
   },
-  {
-    label: 'Selection',
-    submenu: [
-      {
-        label: 'Undo',
-        click(item, focusedWindow) {
-          const options = {
-            type: 'info',
-            buttons: ['OK'],
-            title: 'Undo Menu Clicked',
-            message: 'Undo menu is clicked!',
-            detail: 'This will be replaced with undo selection of item(s) in treeview.'
-          };
-          dialog.showMessageBox(focusedWindow, options);
-        }
-      },
-      {
-        label: 'Redo',
-        click(item, focusedWindow) {
-          const options = {
-            type: 'info',
-            buttons: ['OK'],
-            title: 'Redo Menu Clicked',
-            message: 'Redo menu is clicked!',
-            detail: 'This will be replaced with redo selection of item(s) in treeview.'
-          };
-          dialog.showMessageBox(focusedWindow, options);
-        }
-      }
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {
-        label: 'Reload',
-        accelerator: 'CmdOrCtrl+R',
-        click(item, focusedWindow) {
-          if (focusedWindow) focusedWindow.reload();
-        },
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'resetzoom'
-      },
-      {
-        role: 'zoomin'
-      },
-      {
-        role: 'zoomout'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'togglefullscreen'
-      }
-    ]
-  },
-  {
-    label: 'Tools',
-    submenu: [
-      {
-        role: 'toggledevtools'
-      }
-    ]
-  }
+  ...commonMenuTemplate
 ];
