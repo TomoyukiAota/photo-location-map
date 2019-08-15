ea4586a650eb52ec5093ba4e517d7b55e6b204cf
Tomoyuki Aota <tomoyukia@gmail.com>
2019-08-15 21:04:37 +0900

    Introduce commonHelpSubmenuTemplate

--- src-main/menu/menu-template-on-windows-linux.ts
+++ src-main/menu/menu-template-on-windows-linux.ts
@@ -1,20 +1,12 @@
 import { dialog, MenuItemConstructorOptions } from 'electron';
-import { commonMenuTemplate } from './common-menu-template';
+import { commonHelpSubmenuTemplate, commonMenuTemplate } from './common-menu-template';
 
 export const menuTemplateOnWindowsLinux: MenuItemConstructorOptions[] = [
   ...commonMenuTemplate,
   {
     label: 'Help',
     submenu: [
-      {
-        label: 'Welcome'
-      },
-      { type: 'separator' },
-      { role: 'toggledevtools' },
-      { type: 'separator' },
-      {
-        label: 'Privacy Statement'
-      }
+      ...commonHelpSubmenuTemplate
     ]
   }
 ];
