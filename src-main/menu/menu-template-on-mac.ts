f27c182daa86ea1135fa862d99d4614aa0e7e34c
Tomoyuki Aota <tomoyukia@gmail.com>
2019-08-15 20:12:22 +0900

    Add the first menu item which contains default submenus on macOS.

--- src-main/menu/menu-template-on-mac.ts
+++ src-main/menu/menu-template-on-mac.ts
@@ -2,6 +2,20 @@ import { dialog, MenuItemConstructorOptions } from 'electron';
 
 export const menuTemplateOnMac: MenuItemConstructorOptions[] = [
   {
+    label: 'The name of the first menu item is the application name on macOS',
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  },
+  {
     label: 'Selection',
     submenu: [
       {
