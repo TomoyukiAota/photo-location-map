5ad382c7b2ff33fc028a9559642f9b5815bd277a
Tomoyuki Aota <tomoyukia@gmail.com>
2019-08-20 01:10:50 +0900

    Set up IPC channel for map change.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { Logger } from '../src-shared/log/logger';
 import { AppModule } from './app/app.module';
 import { AppConfig } from './environments/environment';
-
-import { Logger } from '../src-shared/log/logger';
+import './ipc-renderer-setup/ipc-map-setup';
 
 if (AppConfig.production) {
   enableProdMode();
