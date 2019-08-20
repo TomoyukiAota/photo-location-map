72a2d685e8cf703da26546b9e3cce567e4115799
Tomoyuki Aota <tomoyukia@gmail.com>
2019-08-20 23:32:15 +0900

    Render the selected map.

--- src/app/map/map/map-type.ts
+++ src/app/map/map/map-type.ts
@@ -2,3 +2,12 @@ export enum MapType {
   GoogleMaps,
   OpenStreetMap
 }
+
+export const getMapType = (mapTypeStr: string) => {
+  switch (mapTypeStr) {
+    case 'GoogleMaps':
+      return MapType.GoogleMaps;
+    case 'OpenStreetMap':
+      return MapType.OpenStreetMap;
+  }
+};
