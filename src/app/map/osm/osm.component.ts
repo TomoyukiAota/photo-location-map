f7ccc6c57ce094e67fd16fa073b792c74432c690
Tomoyuki Aota <tomoyukia@gmail.com>
2019-08-22 23:26:34 +0900

    Display OSM.

--- src/app/map/osm/osm.component.ts
+++ src/app/map/osm/osm.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-osm',
   templateUrl: './osm.component.html',
   styleUrls: ['./osm.component.scss']
 })
-export class OsmComponent implements OnInit {
+export class OsmComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    const map = L.map('osm').setView([51.505, -0.09], 13);
+    const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      maxZoom: 19
+    });
+    tileLayer.addTo(map);
+    this.changeDetectorRef.detectChanges();
   }
 
 }
