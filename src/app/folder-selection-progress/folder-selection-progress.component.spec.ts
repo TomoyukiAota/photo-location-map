52d1128e6a141bde1329d43e32a50c2e0b3f53ff
Tomoyuki Aota <tomoyukia@gmail.com>
2019-09-02 22:45:05 +0900

    Show the dialog for folder selection progress.

---
+++ src/app/folder-selection-progress/folder-selection-progress.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FolderSelectionProgressComponent } from './folder-selection-progress.component';
+
+describe('FolderSelectionProgressComponent', () => {
+  let component: FolderSelectionProgressComponent;
+  let fixture: ComponentFixture<FolderSelectionProgressComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FolderSelectionProgressComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FolderSelectionProgressComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/folder-selection-progress/folder-selection-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-folder-selection-progress',
+  templateUrl: './folder-selection-progress.component.html',
+  styleUrls: ['./folder-selection-progress.component.scss']
+})
+export class FolderSelectionProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
