69adfaf61633a5454522909fd1301780a44ce296
Tomoyuki Aota <tomoyukia@gmail.com>
2019-09-03 00:02:21 +0900

    Display progress spinner and text on folder selection progress dialog.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule, MatSpinner } from '@angular/material/progress-spinner';
 import { MatTreeModule } from '@angular/material/tree';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -48,7 +49,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     OsmComponent
   ],
   entryComponents: [
-    FolderSelectionProgressComponent
+    FolderSelectionProgressComponent,
+    MatSpinner
   ],
   imports: [
     BrowserModule,
@@ -70,6 +72,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatProgressSpinnerModule,
     MatTreeModule
   ],
   bootstrap: [AppComponent]
