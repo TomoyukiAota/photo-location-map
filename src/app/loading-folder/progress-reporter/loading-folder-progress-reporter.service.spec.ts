08f51123970c4742503e968c314243b3ce31a9a6
Tomoyuki Aota <tomoyukia@gmail.com>
2019-09-03 23:17:51 +0900

    Update test description.

--- src/app/loading-folder/progress-reporter/loading-folder-progress-reporter.service.spec.ts
+++ src/app/loading-folder/progress-reporter/loading-folder-progress-reporter.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { LoadingFolderProgressReporterService } from './loading-folder-progress-reporter.service';
 
-describe('FolderSelectionProgressReporterService', () => {
+describe('LoadingFolderProgressReporterService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
