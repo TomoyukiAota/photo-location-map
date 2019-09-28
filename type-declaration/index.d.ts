6908d310e23e502a5f4b1c3ca4a148a353713307
Tomoyuki Aota <tomoyukia@gmail.com>
2019-09-29 03:33:11 +0900

    Extract window.d.ts

--- type-declaration/index.d.ts
+++ type-declaration/index.d.ts
@@ -1,24 +1,3 @@
-declare var window: Window;
-
-interface AboutBoxShowable {
-  showAboutBox(): void;
-}
-
-interface MapChangeable {
-  changeMap(ipcMapChangeArg: string): void;
-}
-
-interface IPlmInternalRenderer {
-  aboutBox: AboutBoxShowable;
-  map: MapChangeable;
-}
-
-interface Window {
-  process: NodeJS.Process;
-  require: NodeRequire;
-  plmInternalRenderer: IPlmInternalRenderer;
-}
-
 type DirectoryTree = ReturnType<typeof import('directory-tree')>;
 
 declare var google: any;
