ffc10561e342b959d4c73d27ad8fcae270a5280f
Tomoyuki Aota <tomoyukia@gmail.com>
2019-10-01 01:58:54 +0900

    Update the content of the about box.

--- src/app/about-box/about-box.component.ts
+++ src/app/about-box/about-box.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ProxyRequire } from '../../../src-shared/require/proxy-require';
+
+const shell = ProxyRequire.electron.shell;
 
 @Component({
   selector: 'app-about-box',
   templateUrl: './about-box.component.html',
   styleUrls: ['./about-box.component.scss']
 })
-export class AboutBoxComponent implements OnInit {
+export class AboutBoxComponent implements AfterViewInit {
+  @ViewChild('gitHubRepoLink', { static: false }) gitHubRepoLink: ElementRef<HTMLAnchorElement>;
+  @ViewChild('authorLink', { static: false }) authorLink: ElementRef<HTMLAnchorElement>;
 
-  constructor() { }
-
-  ngOnInit() {
+  public ngAfterViewInit() {
+    this.configureOpeningInOsBrowser(this.gitHubRepoLink, 'https://github.com/TomoyukiAota/photo-location-map');
+    this.configureOpeningInOsBrowser(this.authorLink, 'https://github.com/tomoyukiaota');
   }
 
+  private configureOpeningInOsBrowser(elementRef: ElementRef<HTMLAnchorElement>, url: string) {
+    elementRef.nativeElement.addEventListener('click', event => {
+      event.preventDefault();
+      // noinspection JSIgnoredPromiseFromCall
+      shell.openExternal(url);
+    });
+  }
 }
