63266daa9a2842bb9ef4b97f5e7af6679212d9e5
Tomoyuki Aota <tomoyukia@gmail.com>
2019-10-03 01:57:06 +0900

    Revise how to fetch version number from package.json.

--- package-test/package-test-info.js
+++ package-test/package-test-info.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const appVersion = require('../package').version;
+const { version } = require('../package.json');
 const getLogDirectory = require('./get-log-directory');
 
 class PackageTestInfo {
@@ -13,18 +13,18 @@ class PackageTestInfo {
     switch(global.process.platform) {
       case 'win32':
         this.packageCreationCommand = 'npm run electron:windows';
-        this.expectedPackageLocation = `${this.releaseDirectory}\\Photo Location Map ${appVersion}.exe`;
-        this.executableLaunchCommand = `"${this.releaseDirectory}\\Photo Location Map ${appVersion}.exe"`;
+        this.expectedPackageLocation = `${this.releaseDirectory}\\Photo Location Map ${version}.exe`;
+        this.executableLaunchCommand = `"${this.releaseDirectory}\\Photo Location Map ${version}.exe"`;
         break;
       case 'darwin':
         this.packageCreationCommand = 'npm run electron:mac';
-        this.expectedPackageLocation = `${this.releaseDirectory}/Photo Location Map-${appVersion}.dmg`;
-        this.executableLaunchCommand = `hdiutil attach "${this.releaseDirectory}/Photo Location Map-${appVersion}.dmg" && open -W "/Volumes/Photo Location Map ${appVersion}/Photo Location Map.app"`;
+        this.expectedPackageLocation = `${this.releaseDirectory}/Photo Location Map-${version}.dmg`;
+        this.executableLaunchCommand = `hdiutil attach "${this.releaseDirectory}/Photo Location Map-${version}.dmg" && open -W "/Volumes/Photo Location Map ${version}/Photo Location Map.app"`;
         break;
       case 'linux':
         this.packageCreationCommand = 'npm run electron:linux';
-        this.expectedPackageLocation = `${this.releaseDirectory}/Photo Location Map-${appVersion}.AppImage`;
-        this.executableLaunchCommand = `"${this.releaseDirectory}/Photo Location Map-${appVersion}.AppImage"`;
+        this.expectedPackageLocation = `${this.releaseDirectory}/Photo Location Map-${version}.AppImage`;
+        this.executableLaunchCommand = `"${this.releaseDirectory}/Photo Location Map-${version}.AppImage"`;
         break;
       default:
         throw new Error(`Unsupported platform for "${__filename}"`);
