33d61249b95476d8a30ae6fe620e03b533c09a2c
Tomoyuki Aota <tomoyukia@gmail.com>
2019-10-03 23:30:03 +0900

    Update the content of Welcome Dialog.

--- src/app/welcome-dialog/welcome-dialog.component.ts
+++ src/app/welcome-dialog/welcome-dialog.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ProxyRequire } from '../../../src-shared/require/proxy-require';
+
+const app = ProxyRequire.electron.remote.app;
 
 @Component({
   selector: 'app-welcome-dialog',
   templateUrl: './welcome-dialog.component.html',
   styleUrls: ['./welcome-dialog.component.scss']
 })
-export class WelcomeDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class WelcomeDialogComponent {
+  public readonly appVersion = app.getVersion();
 }
