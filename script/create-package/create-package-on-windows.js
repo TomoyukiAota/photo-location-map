96442718085321dfc5fa835e6b099f24dd6d68a5
Tomoyuki Aota <tomoyukia@gmail.com>
2019-10-07 01:34:57 +0900

    Include running electron-builder in create-package-on-window.js

--- script/create-package/create-package-on-windows.js
+++ script/create-package/create-package-on-windows.js
@@ -1,23 +1,46 @@
+const child_process = require('child_process');
 const fsExtra = require('fs-extra');
 const archiver = require('archiver');
 
 const repoRootDir = `${__dirname}\\..\\..`;
 const { version } = require(`${repoRootDir}\\package.json`);
 const exeFilePathFromElectronBuilder = `${repoRootDir}\\release\\Photo Location Map ${version}.exe`;
-const packageDir = `${repoRootDir}\\package`;
-const zipFilePath = `${packageDir}\\Photo Location Map ${version}.zip`;
-const exeFileNameInZipFile = 'Photo Location Map.exe';
 
-fsExtra.ensureDirSync(packageDir);
+const createExeFile = () => {
+  console.info(`Started creating an EXE file "${exeFilePathFromElectronBuilder}".`);
 
-const archive = archiver('zip', { zlib: { level: 9 } });
-archive.on('warning', function(err) { throw err; });
-archive.on('error', function(err) { throw err; });
+  const commandToCreateExeFile = 'npm run electron:windows';
+  const stdout = child_process.execSync(commandToCreateExeFile);
+  console.info(stdout.toString());
 
-const output = fsExtra
-  .createWriteStream(zipFilePath)
-  .on('close', () => console.log(`Finished creating ${zipFilePath} (${archive.pointer()} bytes).`));
+  console.info(`Finished creating an EXE file "${exeFilePathFromElectronBuilder}".`);
+};
 
-archive.pipe(output);
-archive.file(exeFilePathFromElectronBuilder, { name: exeFileNameInZipFile });
-archive.finalize();
+const createZipFile = () => {
+  const packageDir = `${repoRootDir}\\package`;
+  const zipFilePath = `${packageDir}\\Photo Location Map ${version}.zip`;
+  const exeFileNameInZipFile = 'Photo Location Map.exe';
+
+  console.info(`Started creating a ZIP file "${zipFilePath}".`);
+
+  fsExtra.ensureDirSync(packageDir);
+
+  const archive = archiver('zip', { zlib: { level: 9 } });
+  archive.on('warning', function(err) { throw err; });
+  archive.on('error', function(err) { throw err; });
+
+  const output = fsExtra
+    .createWriteStream(zipFilePath)
+    .on('close', () => console.log(`Finished creating a ZIP file "${zipFilePath}" (${archive.pointer()} bytes).`));
+
+  archive.pipe(output);
+  archive.file(exeFilePathFromElectronBuilder, { name: exeFileNameInZipFile });
+  archive.finalize();
+};
+
+const createPackageOnWindows = () => {
+  createExeFile();
+  createZipFile();
+};
+
+createPackageOnWindows();
