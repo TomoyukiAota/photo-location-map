14501e8d7e33277eb5673acdec0117dde396f4be
Tomoyuki Aota <tomoyukia@gmail.com>
2019-10-08 23:54:39 +0900

    Move logger.js and run-command-sync.js. User logger in package creation scripts. Use run-command-sync in package creation test.

--- script/package-test/run-package-test.js
+++ script/package-test/run-package-test.js
@@ -1,4 +1,4 @@
-const logger = require('../logger');
+const logger = require('../util/logger');
 const packageCreationTest = require('./package-creation-test');
 const packageSmokeTest = require('./package-smoke-test');
 
