27508e22bf376e60afbada69b580eef188c7aee8
Tomoyuki Aota <tomoyukia@gmail.com>
2019-11-08 02:47:53 +0900

    Refactor notarize.js

--- script/notarize/notarize.js
+++ script/notarize/notarize.js
@@ -3,27 +3,26 @@ const logger = require('../util/logger');
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
+  const appName = context.packager.appInfo.productFilename;
 
-  if (electronPlatformName !== 'darwin') {
+  const isMac = electronPlatformName === 'darwin';
+  if (!isMac) {
     logger.info('Notarization is skipped on OSs other than macOS.');
     return;
   }
 
-  if (process.env.PLM_PACKAGE_TEST) {
+  const isPackageTest = !!process.env.PLM_PACKAGE_TEST;
+  if (isPackageTest) {
     logger.info('Notarization is skipped in package test.');
     return;
   }
 
   logger.info('Started notarization.');
-
-  const appName = context.packager.appInfo.productFilename;
-
   await notarize({
     appBundleId: 'com.TomoyukiAota.PhotoLocationMap',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
   });
-
   logger.info('Finished notarization.');
 };
