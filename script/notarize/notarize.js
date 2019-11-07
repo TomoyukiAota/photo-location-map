da72887f1b9219d309e4b0c1adfdefd011f82adf
Tomoyuki Aota <tomoyukia@gmail.com>
2019-11-08 02:20:41 +0900

    Skip notarization in package test.

--- script/notarize/notarize.js
+++ script/notarize/notarize.js
@@ -1,10 +1,20 @@
 const { notarize } = require('electron-notarize');
+const logger = require('../util/logger');
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
+
   if (electronPlatformName !== 'darwin') {
+    logger.info('Notarization is skipped on OSs other than macOS.');
     return;
   }
+
+  if (process.env.PLM_PACKAGE_TEST) {
+    logger.info('Notarization is skipped in package test.');
+    return;
+  }
+
+  logger.info('Started notarization.');
 
   const appName = context.packager.appInfo.productFilename;
 
