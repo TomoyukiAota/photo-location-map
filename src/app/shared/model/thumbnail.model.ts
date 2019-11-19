d527ae7011818679a22055f2c831012a9bda2eca
Tomoyuki Aota <tomoyukia@gmail.com>
2019-11-20 02:07:37 +0900

    Prepare the location where thumbnail generation happens when thumbnail is missing in EXIF.

--- src/app/shared/model/thumbnail.model.ts
+++ src/app/shared/model/thumbnail.model.ts
@@ -9,14 +9,36 @@ export class Thumbnail {
 
   public static async create(photo: Photo): Promise<Thumbnail> {
     const exif = photo.exifParserResult;
-    if (exif === null || !exif.hasThumbnail('image/jpeg'))
+    const thumbnailGenerationRequired = !exif || !(exif.hasThumbnail('image/jpeg'));
+    const base64String = thumbnailGenerationRequired
+      ? await this.createBase64StringFromFile(photo)
+      : await this.createBase64StringFromExif(exif);
+
+    if(!base64String)
       return null;
 
-    const buffer = exif.getThumbnailBuffer();
-    const base64String = btoa(String.fromCharCode.apply(null, buffer));
     const dataUrl = `data:image/jpg;base64,${base64String}`;
     const rotated = await imageRotator.correctRotation(dataUrl, exif.tags.Orientation);
     const rotatedDimensions = new Dimensions(rotated.width, rotated.height);
     return new Thumbnail(rotated.dataUrl, rotatedDimensions);
+  }
+
+  private static async createBase64StringFromExif(exif): Promise<string> {
+    const buffer = exif.getThumbnailBuffer();
+    const base64String = btoa(String.fromCharCode.apply(null, buffer));
+    return base64String;
+  }
+
+  private static async createBase64StringFromFile(photo: Photo): Promise<string | null> {
+    try {
+      // TODO: get base64 string of shrunk image
+      // const base64String: string =
+      // console.log(base64String);
+      // return base64String;
+      return null;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
   }
 }
