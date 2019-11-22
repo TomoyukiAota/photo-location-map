ba5dd2c95338d09a91634a8f6c8c201bd016d45e
Tomoyuki Aota <tomoyukia@gmail.com>
2019-11-22 23:02:36 +0900

    Bump version number of download link to 0.2.0

--- docs/github-pages/download.js
+++ docs/github-pages/download.js
@@ -8,8 +8,8 @@ const download = (url) => {
 };
 
 const downloadLink = {
-  win: 'https://github.com/TomoyukiAota/photo-location-map/releases/download/v0.1.0/Photo-Location-Map-Setup-0.1.0.exe',
-  mac: 'https://github.com/TomoyukiAota/photo-location-map/releases/download/v0.1.0/Photo-Location-Map-0.1.0.dmg'
+  win: 'https://github.com/TomoyukiAota/photo-location-map/releases/download/v0.2.0/Photo-Location-Map-Setup-0.2.0.exe',
+  mac: 'https://github.com/TomoyukiAota/photo-location-map/releases/download/v0.2.0/Photo-Location-Map-0.2.0.dmg'
 };
 
 const downloadForWindows = () => download(downloadLink.win);
