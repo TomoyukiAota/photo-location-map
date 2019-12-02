fe0bc436e8fbecbe24333bfa91b4852b20f1a1a9
Tomoyuki Aota <tomoyukia@gmail.com>
2019-12-03 00:43:54 +0900

    Rename to DevOrProdType.

--- src-shared/dev-or-prod/dev-or-prod.ts
+++ src-shared/dev-or-prod/dev-or-prod.ts
@@ -1,15 +1,15 @@
 import { Logger } from '../log/logger';
 
-export type DevOrProd = 'Dev' | 'Prod';
+export type DevOrProdType = 'Dev' | 'Prod';
 
-const fetchDevOrProdFromConfig: (() => DevOrProd) = () => {
+const fetchDevOrProdFromConfig: (() => DevOrProdType) = () => {
   // devOrProdFromConfig is set from "config" property in package.json
   // Also, package.json does not exist in the packaged application.
   // This means the followings:
   //  - If npm script launches non-packaged application (e.g. npm start), devOrProdFromConfig is set.
   //  - If npm script launches packaged application (i.e. package-test), devOrProdFromConfig is set.
   //  - If packaged application is directly launched (i.e. publicly released application), devOrProdFromConfig is not set (undefined).
-  const devOrProdFromConfig = process.env.npm_package_config_devOrProd as DevOrProd;
+  const devOrProdFromConfig = process.env.npm_package_config_devOrProd as DevOrProdType;
 
   Logger.info(`DevOrProd from "config" property in package.json: ${devOrProdFromConfig}`);
 
@@ -27,6 +27,6 @@ const fetchDevOrProdFromConfig: (() => DevOrProd) = () => {
 const devOrProd = fetchDevOrProdFromConfig();
 Logger.info(`Determined DevOrProd: ${devOrProd}`);
 
-export const getDevOrProd: (() => DevOrProd) = () => {
+export const getDevOrProd: (() => DevOrProdType) = () => {
   return devOrProd;
 };
