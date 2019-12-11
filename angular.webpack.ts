1724e7d43562bf0313a8c572db73cf8b6168a702
Tomoyuki Aota <tomoyukia@gmail.com>
2019-12-12 03:22:03 +0900

    Add static typing in angular.webpack.ts

--- angular.webpack.ts
+++ angular.webpack.ts
@@ -1,8 +1,9 @@
+import { CustomWebpackBrowserSchema } from '@angular-builders/custom-webpack';
+import * as webpack from 'webpack';
+
 // This is Custom Webpack Config Function.
 // See https://www.npmjs.com/package/@angular-builders/custom-webpack#custom-webpack-config-function
-// config: Webpack Configuration Object
-// options: "options" in angular.json
-module.exports = (config, options) => {
+export default (config: webpack.Configuration, options: CustomWebpackBrowserSchema) => {
   config.target = 'electron-renderer';
   return config;
 };
