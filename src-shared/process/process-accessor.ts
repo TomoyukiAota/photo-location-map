9c28ba7ff4ee89c9bb01f18e0b2ecdb962bbcf07
Tomoyuki Aota <tomoyukia@gmail.com>
2019-12-12 02:44:09 +0900

    Use target: electron-renderer for unit tests by Karma.

--- src-shared/process/process-accessor.ts
+++ src-shared/process/process-accessor.ts
@@ -1,15 +1,26 @@
-import { EnvironmentDetector } from '../environment/environment-detector';
-
 export class ProcessAccessor {
   public static getProcess(): NodeJS.Process {
-    // In Karma, process variable is overridden like following:
-    // "{"title":"browser","browser":true,"env":{},"argv":[],"version":"","versions":{}}'"
-    // window.process is kept the same as the one from Electron, so use window.process for Karma.
-    // Also, Karma is for tests on Electron renderer process, so window variable must exist.
-    // In cases other than Karma, process variable exists both on main and renderer process.
-
-    return EnvironmentDetector.isKarma
-         ? window.process
-         : process;
+    return process;
   }
 }
+
+
+
+// Before introducing @angular-builders/custom-webpack, there was an issue that process variable is overridden in Karma environment.
+// Therefore, ProcessAccessor.getProcess() is implemented as below.
+
+// import { EnvironmentDetector } from '../environment/environment-detector';
+//
+// export class ProcessAccessor {
+//   public static getProcess(): NodeJS.Process {
+//     // In Karma, process variable is overridden like following:
+//     // "{"title":"browser","browser":true,"env":{},"argv":[],"version":"","versions":{}}'"
+//     // window.process is kept the same as the one from Electron, so use window.process for Karma.
+//     // Also, Karma is for tests on Electron renderer process, so window variable must exist.
+//     // In cases other than Karma, process variable exists both on main and renderer process.
+//
+//     return EnvironmentDetector.isKarma
+//          ? window.process
+//          : process;
+//   }
+// }
