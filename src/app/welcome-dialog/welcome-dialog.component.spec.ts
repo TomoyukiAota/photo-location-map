574320a328334afcdccd4ea58e225f1598633a21
Tomoyuki Aota <tomoyukia@gmail.com>
2020-10-13 00:19:01 +0900

    Migrate to waitForAsync from async.

--- src/app/welcome-dialog/welcome-dialog.component.spec.ts
+++ src/app/welcome-dialog/welcome-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WelcomeDialogComponent } from './welcome-dialog.component';
 
@@ -6,7 +6,7 @@ describe('WelcomeDialogComponent', () => {
   let component: WelcomeDialogComponent;
   let fixture: ComponentFixture<WelcomeDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WelcomeDialogComponent ]
     })
