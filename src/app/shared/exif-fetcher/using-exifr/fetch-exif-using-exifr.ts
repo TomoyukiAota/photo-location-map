565a894a7d867e3940a9e4e1eea71183831bb57c
Tomoyuki Aota <tomoyukia@gmail.com>
2020-10-16 02:40:31 +0900

    Add a comment about the reason why exifr in the main process is used.

--- src/app/shared/exif-fetcher/using-exifr/fetch-exif-using-exifr.ts
+++ src/app/shared/exif-fetcher/using-exifr/fetch-exif-using-exifr.ts
@@ -6,6 +6,7 @@ import { LatLng } from '../../model/lat-lng.model';
 import { Thumbnail } from '../../model/thumbnail.model';
 import * as imageRotator from '../../image-rotator';
 
+// exifr in the main process is used because it runs faster than the one in the renderer process.
 const exifr: typeof import('exifr') = window.require('electron').remote.require('exifr');
 
 export function fetchExifUsingExifr(filePath: string): Promise<Exif> {
