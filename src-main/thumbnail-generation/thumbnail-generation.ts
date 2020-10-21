cfe91ef1dc9c6b2fcf5b0d8b090ee9e40d563137
Tomoyuki Aota <tomoyukia@gmail.com>
2020-10-22 03:02:54 +0900

    Check if the file for worker thread used during thumbnail generation is found or not.

--- src-main/thumbnail-generation/thumbnail-generation.ts
+++ src-main/thumbnail-generation/thumbnail-generation.ts
@@ -1,13 +1,34 @@
 import { DirectoryTree } from 'directory-tree';
 import { ipcMain } from 'electron';
+import * as fs from 'fs';
 import { spawn, Thread, Worker } from 'threads';
 import * as pathModule from 'path';
 import * as allSettled from 'promise.allsettled';
 
-import { IpcConstants } from '../../src-shared/ipc/ipc-constants';
 import { convertToFlattenedDirTree } from '../../src-shared/dir-tree/dir-tree-util';
+import { IpcConstants } from '../../src-shared/ipc/ipc-constants';
+import { Logger } from '../../src-shared/log/logger';
 import { FilenameExtension } from '../../src-shared/filename-extension/filename-extension';
 import { ThumbnailFileGenerationArgs } from './generate-thumbnail-file-arg-and-result';
+
+
+class FileForWorkerThread {
+  private static _relativePathWithoutExtension = './generate-thumbnail-file-worker';
+  public static get relativePathWithoutExtension() { return this._relativePathWithoutExtension; }
+  public static get relativePathWithExtension() { return `${this._relativePathWithoutExtension}.js`; }
+}
+
+function checkFileForWorkerThreadExists(): void {
+  const absoluteFilePathForWorkerThread = pathModule.join(__dirname, FileForWorkerThread.relativePathWithExtension);
+  Logger.info(`The expected file path for worker thread used during thumbnail generation is "${absoluteFilePathForWorkerThread}"`);
+  const fileExists = fs.existsSync(absoluteFilePathForWorkerThread);
+
+  if (fileExists) {
+    Logger.info(`The file for worker thread used during thumbnail generation is found.`);
+  } else {
+    Logger.error(`The file for worker thread used during thumbnail generation is NOT found.`);
+  }
+}
 
 function getThumbnailOutputPath(filePath: string) {
   const parsedPath = pathModule.parse(filePath);
@@ -25,7 +46,7 @@ async function generateThumbnails(heifFiles: DirectoryTree[]) {
   const promiseArray = heifFiles.map(async file => {
     console.log(`heifFile`, file);
 
-    const generateThumbnailFile = await spawn(new Worker('./generate-thumbnail-file-worker'));
+    const generateThumbnailFile = await spawn(new Worker(FileForWorkerThread.relativePathWithoutExtension));
 
     const args = new ThumbnailFileGenerationArgs();
     args.srcFilePath = file.path;
@@ -50,6 +71,8 @@ ipcMain.handle(IpcConstants.ThumbnailGenerationInMainProcess.Name, (event, direc
   const heifFiles = flattenedDirTree.filter(element => FilenameExtension.isHeif(element.extension));
   console.log(`heifFiles`);
   console.log(heifFiles);
+
+  checkFileForWorkerThreadExists();
 
   // noinspection JSUnusedLocalSymbols
   const ignoredPromise = generateThumbnails(heifFiles);    // Promise is deliberately ignored.
