8fc96f31b709e0b81aa598e6bd57e16b24c6d5a9
Tomoyuki Aota <tomoyukia@gmail.com>
2020-10-24 00:49:57 +0900

    Extract code related to thumbnail element into a separate file.

--- src/app/photo-info-viewer/photo-info-viewer-content.ts
+++ src/app/photo-info-viewer/photo-info-viewer-content.ts
@@ -1,16 +1,9 @@
-import * as fs from 'fs';
-import { Analytics } from '../../../src-shared/analytics/analytics';
-import { FilenameExtension } from '../../../src-shared/filename-extension/filename-extension';
-import { Logger } from '../../../src-shared/log/logger';
-import { IconDataUrl } from '../../assets/icon-data-url';
-import { Dimensions } from '../shared/model/dimensions.model';
 import { Photo } from '../shared/model/photo.model';
-import { PhotoViewerLauncher } from '../photo-viewer/photo-viewer-launcher';
-import { OpenContainingFolderIconElement } from './open-containing-folder-icon-element';
-import { RotateIconElement } from './rotate-icon-element';
 import { LaunchPhotoViewerIconElement } from './launch-photo-viewer-icon-element';
+import { OpenContainingFolderIconElement } from './open-containing-folder-icon-element';
 import { PlayLivePhotosIconElement } from './play-live-photos-icon-element';
-import { getThumbnailFilePath } from '../../../src-shared/thumbnail/get-thumbnail-file-path';
+import { RotateIconElement } from './rotate-icon-element';
+import { ThumbnailElement } from './thumbnail-element';
 
 export class PhotoInfoViewerContent {
   public static generate(photo: Photo) {
@@ -27,8 +20,7 @@ export class PhotoInfoViewerContent {
   }
 
   private static appendToRootDivElement(rootDivElement, photo: Photo) {
-    const thumbnailElement = this.createThumbnailElement(photo);
-    const thumbnailContainerElement = this.createThumbnailContainerElement(thumbnailElement);
+    const { thumbnailElement, thumbnailContainerElement } = ThumbnailElement.create(photo);
     const nameElement = this.createNameElement(photo);
     const dateTakenElement = this.createDateTimeTakenElement(photo);
 
@@ -39,104 +31,6 @@ export class PhotoInfoViewerContent {
     this.appendOpenContainingFolderIconElement(rootDivElement, photo);
     this.appendLaunchPhotoViewerIconElement(rootDivElement, photo);
     this.appendPlayLivePhotosIconElement(rootDivElement, photo);
-  }
-
-  private static createThumbnailElement(photo: Photo) {
-    const thumbnailElement = document.createElement('img');
-
-    const isThumbnailAvailableFromExif = photo.exif.thumbnail;
-    const isPhotoDisplayableInBrowser = FilenameExtension.isDisplayableInBrowser(photo.filenameExtension);
-    const isThumbnailGenerationAvailable = FilenameExtension.isThumbnailGenerationAvailable(photo.filenameExtension);
-
-    if (isThumbnailAvailableFromExif) {
-      this.displayThumbnailFromExif(thumbnailElement, photo);
-    } else if (isPhotoDisplayableInBrowser) {
-      this.displayThumbnailUsingPhotoItself(thumbnailElement, photo);
-    } else if (isThumbnailGenerationAvailable) {
-      this.displayGeneratedThumbnail(thumbnailElement, photo);
-    } else {
-      this.displayNoThumbnailAvailableImage(thumbnailElement, photo);
-    }
-
-    thumbnailElement.style.transition = 'transform 0.3s ease-in-out';
-    thumbnailElement.onclick = () => this.handleThumbnailClick(photo);
-    return thumbnailElement;
-  }
-
-  private static displayThumbnailFromExif(thumbnailElement: HTMLImageElement, photo: Photo) {
-    thumbnailElement.src = photo.exif.thumbnail.dataUrl;
-    thumbnailElement.width = photo.exif.thumbnail.dimensions.width;
-    thumbnailElement.height = photo.exif.thumbnail.dimensions.height;
-    thumbnailElement.title = `Click the thumbnail to open ${photo.name}`;
-  }
-
-  private static displayThumbnailUsingPhotoItself(thumbnailElement: HTMLImageElement, photo: Photo) {
-    this.displayThumbnailUsingFile(thumbnailElement, photo, photo.path);
-  }
-
-  private static displayGeneratedThumbnail(thumbnailElement: HTMLImageElement, photo: Photo) {
-    const intervalId = setInterval(() => {
-      const { thumbnailFilePath } = getThumbnailFilePath(photo.path);
-      const thumbnailFileExists = fs.existsSync(thumbnailFilePath);
-      if (thumbnailFileExists) {
-        this.displayThumbnailUsingFile(thumbnailElement, photo, thumbnailFilePath);
-        clearInterval(intervalId);
-      } else {
-        this.displayGeneratingThumbnailImage(thumbnailElement, photo);
-      }
-    }, 1000);
-  }
-
-  private static minThumbnailContainerSquareSideLength = 200;
-
-  private static displayThumbnailUsingFile(thumbnailElement: HTMLImageElement, photo: Photo, thumbnailFilePath: string) {
-    // # needs to be escaped. See https://www.w3schools.com/tags/ref_urlencode.asp for encoding.
-    const escapedPath = thumbnailFilePath.replace(/#/g, '%23');
-    thumbnailElement.src = `file://${escapedPath}`;
-    const largerSideLength = this.minThumbnailContainerSquareSideLength;
-    if (photo.exif.imageDimensions.width > photo.exif.imageDimensions.height) {
-      thumbnailElement.width = largerSideLength;
-      thumbnailElement.height = largerSideLength * (photo.exif.imageDimensions.height / photo.exif.imageDimensions.width);
-    } else {
-      thumbnailElement.width = largerSideLength * (photo.exif.imageDimensions.width / photo.exif.imageDimensions.height);
-      thumbnailElement.height = largerSideLength;
-    }
-    thumbnailElement.title = `Click the thumbnail to open ${photo.name}`;
-  }
-
-  private static displayGeneratingThumbnailImage(thumbnailElement: HTMLImageElement, photo: Photo) {
-    thumbnailElement.width = 150;
-    thumbnailElement.height = 15;
-    thumbnailElement.src = IconDataUrl.generatingThumbnail;
-    thumbnailElement.title = `Generating thumbnail for ${photo.name}.`;
-  }
-
-  private static displayNoThumbnailAvailableImage(thumbnailElement: HTMLImageElement, photo: Photo) {
-    thumbnailElement.width = 150;
-    thumbnailElement.height = 15;
-    thumbnailElement.src = IconDataUrl.noThumbnailAvailable;
-    thumbnailElement.title = `Thumbnail is not available for ${photo.name}.`;
-  }
-
-  private static handleThumbnailClick(photo: Photo): void {
-    Logger.info(`Photo Info Viewer: Clicked the thumbnail of ${photo.path}`);
-    Analytics.trackEvent('Photo Info Viewer', 'Clicked Thumbnail');
-    PhotoViewerLauncher.launch(photo);
-  }
-
-  private static createThumbnailContainerElement(thumbnailElement: HTMLImageElement) {
-    const thumbnailContainer = document.createElement('div');
-    thumbnailContainer.style.display = 'flex';
-    thumbnailContainer.style.justifyContent = 'center';
-    thumbnailContainer.style.alignItems = 'center';
-    const thumbnailContainerDimensions = new Dimensions(thumbnailElement.width, thumbnailElement.height).expandToSquare();
-    thumbnailContainer.style.width = thumbnailContainerDimensions.width.toString() + 'px';
-    thumbnailContainer.style.minWidth = `${this.minThumbnailContainerSquareSideLength}px`;
-    thumbnailContainer.style.height = thumbnailContainerDimensions.height.toString() + 'px';
-    thumbnailContainer.style.minHeight = `${this.minThumbnailContainerSquareSideLength}px`;
-
-    thumbnailContainer.appendChild(thumbnailElement);
-    return thumbnailContainer;
   }
 
   private static createNameElement(photo: Photo) {
