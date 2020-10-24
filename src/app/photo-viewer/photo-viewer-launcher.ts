fdc42e328f43fa98e5454b4b2b3e6bb3be89a75b
Tomoyuki Aota <tomoyukia@gmail.com>
2020-10-25 04:23:29 +0900

    Log warning when file path is too long on Windows.

--- src/app/photo-viewer/photo-viewer-launcher.ts
+++ src/app/photo-viewer/photo-viewer-launcher.ts
@@ -1,10 +1,10 @@
 import { Command } from '../../../src-shared/command/command';
 import { Logger } from '../../../src-shared/log/logger';
+import { isFilePathTooLongOnWindows, maxFilePathLengthOnWindows } from '../../../src-shared/max-file-path-length-on-windows/max-file-path-length-on-windows';
 import { Photo } from '../shared/model/photo.model';
 import { FallbackPhotoViewer } from './fallback-photo-viewer/fallback-photo-viewer';
 
 const child_process = window.require('child_process');
-const os = window.require('os');
 
 export class PhotoViewerLauncher {
   public static launch(photo: Photo): void {
@@ -20,6 +20,12 @@ export class PhotoViewerLauncher {
     child_process.spawn(command, [], { shell: true });
     Logger.info(`Issued a command: ${command}`);
     Logger.info(`Opened ${photo.path}`, photo);
+
+    if (isFilePathTooLongOnWindows(photo.path)) {
+      Logger.warn(`"Open ${photo.name}" might not work because the length of the file path exceeds the maximum on Windows.\n`
+        + `Maximum: ${maxFilePathLengthOnWindows} characters\n`
+        + `Photo path: ${photo.path.length} characters`);
+    }
   }
 
   private static launchFallbackPhotoViewer(photo: Photo): void {
