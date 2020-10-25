b059d65b87e74e3d33a6c074ecab3db3eaca8065
Tomoyuki Aota <tomoyukia@gmail.com>
2020-10-25 23:10:09 +0900

    Refactor asyncMap

--- src-shared/async-util/async-util.ts
+++ src-shared/async-util/async-util.ts
@@ -1,6 +1,14 @@
-// Modified based on https://qiita.com/janus_wel/items/1dc491d866f49af76e98
-export async function asyncMap<TInput, TOutput>(array: TInput[], operation: (item: TInput) => Promise<TOutput>): Promise<TOutput[]> {
-  return await Promise.all(array.map(async item => await operation(item)));
+// For asyncMap and asyncFilter, see https://qiita.com/janus_wel/items/1dc491d866f49af76e98
+
+
+/**
+ * Maps inputArray to outputArray using mapFn asynchronously.
+ * Note that mapFn must always resolve.
+ * This function does not correctly work when rejection occurs in mapFn.
+ */
+export async function asyncMap<TInput, TOutput>(inputArray: TInput[], mapFn: (item: TInput) => Promise<TOutput>): Promise<TOutput[]> {
+  const outputPromiseArray = inputArray.map(async item => await mapFn(item));
+  return await Promise.all(outputPromiseArray);
 }
 
 // Modified based on https://qiita.com/janus_wel/items/1dc491d866f49af76e98
