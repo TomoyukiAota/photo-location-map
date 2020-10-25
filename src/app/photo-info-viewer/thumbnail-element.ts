f7fe184739f89389e3dc0607e783f4813dabc9c3
Tomoyuki Aota <tomoyukia@gmail.com>
2020-10-26 00:01:00 +0900

    Check thumbnail file cache availability before displaying thumbnail.

--- src/app/photo-info-viewer/thumbnail-element.ts
+++ src/app/photo-info-viewer/thumbnail-element.ts
@@ -1,9 +1,8 @@
-import * as fs from 'fs';
 import { Analytics } from '../../../src-shared/analytics/analytics';
 import { FilenameExtension } from '../../../src-shared/filename-extension/filename-extension';
 import { Logger } from '../../../src-shared/log/logger';
 import { isFilePathTooLongOnWindows, maxFilePathLengthOnWindows } from '../../../src-shared/max-file-path-length-on-windows/max-file-path-length-on-windows';
-import { getThumbnailFilePath } from '../../../src-shared/thumbnail/thumbnail-generation-util';
+import { getThumbnailFilePath, isThumbnailCacheAvailable } from '../../../src-shared/thumbnail/thumbnail-generation-util';
 import { IconDataUrl } from '../../assets/icon-data-url';
 import { Dimensions } from '../shared/model/dimensions.model';
 import { Photo } from '../shared/model/photo.model';
@@ -55,9 +54,8 @@ export class ThumbnailElement {
 
   private static displayGeneratedThumbnail(thumbnailElement: HTMLImageElement, photo: Photo) {
     const intervalId = setInterval(() => {
-      const { thumbnailFilePath } = getThumbnailFilePath(photo.path);
-      const thumbnailFileExists = fs.existsSync(thumbnailFilePath);
-      if (thumbnailFileExists) {
+      if (isThumbnailCacheAvailable(photo.path)) {
+        const { thumbnailFilePath } = getThumbnailFilePath(photo.path);
         this.displayThumbnailUsingFile(thumbnailElement, photo, thumbnailFilePath);
         this.handlePathTooLongCaseOnWindowsForGeneratedThumbnail(thumbnailElement, photo, thumbnailFilePath);
         clearInterval(intervalId);
