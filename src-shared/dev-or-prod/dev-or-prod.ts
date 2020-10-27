4d77036cea2c1f6f6b5f10f67c3c6db3d5f51195
Tomoyuki Aota <tomoyukia@gmail.com>
2020-10-27 21:10:59 +0900

    Treat as development environment for alpha version.

--- src-shared/dev-or-prod/dev-or-prod.ts
+++ src-shared/dev-or-prod/dev-or-prod.ts
@@ -1,30 +1,43 @@
 import { Logger } from '../log/logger';
+import { ProxyRequire } from '../require/proxy-require';
+import { RequireFromMainProcess } from '../require/require-from-main-process';
 
 export type DevOrProdType = 'Dev' | 'Prod';
 
-const fetchDevOrProdFromConfig: (() => DevOrProdType) = () => {
-  // devOrProdFromConfig is set from "config" property in package.json
-  // Also, package.json does not exist in the packaged application.
+function determineDevOrProd(): DevOrProdType {
+  // process.env.npm_package_config_devOrProd is set from "devOrProd" in "config" in package.json
+  // when npm script defined in package.json is used.
   // This means the followings:
-  //  - If npm script launches non-packaged application (e.g. npm start), devOrProdFromConfig is set.
-  //  - If npm script launches packaged application (i.e. package-test), devOrProdFromConfig is set.
-  //  - If packaged application is directly launched (i.e. publicly released application), devOrProdFromConfig is not set (undefined).
-  const devOrProdFromConfig = process.env.npm_package_config_devOrProd as DevOrProdType;
+  //  - If npm script (e.g. "npm start", "npm run test:all") is used, devOrProdInPackageJson is set to the value of "devOrProd" in package.json
+  //  - If packaged application is directly launched (i.e. publicly released application), devOrProdInPackageJson is not set (undefined).
+  const devOrProdInPackageJson = process.env.npm_package_config_devOrProd as DevOrProdType;
 
-  Logger.info(`DevOrProd from "config" property in package.json: ${devOrProdFromConfig}`);
+  Logger.info(`"devOrProd" in "config" in package.json: ${devOrProdInPackageJson}`);
 
-  if (devOrProdFromConfig === 'Dev') {
-    return 'Dev';
-  } else if (devOrProdFromConfig === 'Prod') {
-    return  'Prod';
-  } else if (!devOrProdFromConfig) {
-    return 'Prod';
+  const isDevOrProdSpecifiedInPackageJson = devOrProdInPackageJson === 'Dev' || devOrProdInPackageJson === 'Prod';
+
+  if (isDevOrProdSpecifiedInPackageJson) {
+    return devOrProdInPackageJson;
+  } else if (!devOrProdInPackageJson) {
+    return determineDevOrProdWhenPackagedAppIsDirectlyLaunched();
   } else {
-    throw new Error(`Either "Dev" or "Prod" needs to be specified "config" property in package.json. Currently, it is "${devOrProdFromConfig}".`);
+    throw new Error(`Either "Dev" or "Prod" needs to be specified for "devOrProd" in "config" in package.json, but the specified value is "${devOrProdInPackageJson}".`);
   }
-};
+}
 
-const devOrProd = fetchDevOrProdFromConfig();
+function determineDevOrProdWhenPackagedAppIsDirectlyLaunched(): DevOrProdType {
+  const appVersion = RequireFromMainProcess?.electron?.app?.getVersion();
+  Logger.info(`Application version to determine DevOrProd: ${appVersion}`);
+  if (!appVersion) {
+    throw new Error(`"devOrProd" in "config" in package.json is falsy. Also, the application version is falsy. Something went wrong.`);
+  }
+
+  const isAlphaVersion = appVersion.toLowerCase().includes('alpha');
+  Logger.info(`Is alpha version? : ${isAlphaVersion}`);
+  return isAlphaVersion ? 'Dev' : 'Prod';
+}
+
+const devOrProd = determineDevOrProd();
 Logger.info(`Determined DevOrProd: ${devOrProd}`);
 
 export class DevOrProd {
