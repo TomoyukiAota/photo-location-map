f6d69b0f7d593e3ee59c807c751a97ed6a8e8c8f
Tomoyuki Aota <tomoyukia@gmail.com>
2020-10-27 23:39:18 +0900

    Close thumbnail generation status display when the folder is reselected.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectionStrategy, AfterViewInit, OnInit } from '@angular/core';
 import Split from 'split.js';
 import { OsmForceRenderService } from '../map/osm/osm-force-render/osm-force-render.service';
+import { FolderSelectionService } from '../shared/service/folder-selection.service';
 import { ThumbnailGenerationService } from '../thumbnail-generation/service/thumbnail-generation.service';
 
 @Component({
@@ -12,10 +13,13 @@ import { ThumbnailGenerationService } from '../thumbnail-generation/service/thum
 export class HomeComponent implements AfterViewInit, OnInit {
   public thumbnailGenerationStatusVisible = false;
 
-  constructor(private thumbnailGenerationService: ThumbnailGenerationService,
+  constructor(private folderSelectionService: FolderSelectionService,
+              private thumbnailGenerationService: ThumbnailGenerationService,
               private osmForceRenderService: OsmForceRenderService) {}
 
   ngOnInit() {
+    this.folderSelectionService.folderSelected.subscribe(
+      () => this.thumbnailGenerationStatusVisible = false);
     this.thumbnailGenerationService.thumbnailGenerationStarted.subscribe(
       () => this.thumbnailGenerationStatusVisible = true);
   }
