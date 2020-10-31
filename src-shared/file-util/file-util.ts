ed61dc3c6a3f416e6a8fe9bc6a165c6e5298ffcb
Tomoyuki Aota <tomoyukia@gmail.com>
2020-10-31 20:28:43 +0900

    Implement the content of CacheSettingsComponent.

--- src-shared/file-util/file-util.ts
+++
@@ -1,7 +0,0 @@
-import * as fs from 'fs';
-
-export async function fileExists(filePath: string): Promise<boolean> {
-  return fs.promises.access(filePath, fs.constants.F_OK)
-    .then(() => true)
-    .catch(() => false);
-}
