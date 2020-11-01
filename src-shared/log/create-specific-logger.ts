19b57d025b3f97b9723a6a13be1297f4e2631f3f
Tomoyuki Aota <tomoyukia@gmail.com>
2020-11-02 00:31:32 +0900

    Add create-specific-logger.ts

---
+++ src-shared/log/create-specific-logger.ts
@@ -0,0 +1,34 @@
+import { Logger } from './logger';
+
+class SpecificLogger {
+  constructor(private stringToPrepend: string) {
+  }
+
+  public error(message: string, ...object: any): void {
+    const text = this.prependLoggerName(message);
+    Logger.error(text, ...object);
+  }
+
+  public warn(message: string, ...object: any): void {
+    const text = this.prependLoggerName(message);
+    Logger.warn(text, ...object);
+  }
+
+  public info(message: string, ...object: any): void {
+    const text = this.prependLoggerName(message);
+    Logger.info(text, ...object);
+  }
+
+  public debug(message: string, ...object: any): void {
+    const text = this.prependLoggerName(message);
+    Logger.debug(text, ...object);
+  }
+
+  private prependLoggerName(text): string {
+    return `${this.stringToPrepend} ${text}`;
+  }
+}
+
+export function createSpecificLogger(stringToPrepend: string) {
+  return new SpecificLogger(stringToPrepend);
+}
