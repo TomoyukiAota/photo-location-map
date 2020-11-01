c9062e5dd3f8fafa1d17bbea3a438918deaa3782
Tomoyuki Aota <tomoyukia@gmail.com>
2020-11-02 00:44:43 +0900

    Fix function name.

--- src-shared/log/create-specific-logger.ts
+++ src-shared/log/create-specific-logger.ts
@@ -5,26 +5,26 @@ class SpecificLogger {
   }
 
   public error(message: string, ...object: any): void {
-    const text = this.prependLoggerName(message);
+    const text = this.prependSpecifiedString(message);
     Logger.error(text, ...object);
   }
 
   public warn(message: string, ...object: any): void {
-    const text = this.prependLoggerName(message);
+    const text = this.prependSpecifiedString(message);
     Logger.warn(text, ...object);
   }
 
   public info(message: string, ...object: any): void {
-    const text = this.prependLoggerName(message);
+    const text = this.prependSpecifiedString(message);
     Logger.info(text, ...object);
   }
 
   public debug(message: string, ...object: any): void {
-    const text = this.prependLoggerName(message);
+    const text = this.prependSpecifiedString(message);
     Logger.debug(text, ...object);
   }
 
-  private prependLoggerName(message: string): string {
+  private prependSpecifiedString(message: string): string {
     return `${this.stringToPrepend} ${message}`;
   }
 }
