533e1a6100ead9978e53d040d2570410e74418ae
Tomoyuki Aota <tomoyukia@gmail.com>
2020-11-17 00:52:50 +0900

    Add Twitter and GitHub profile icons in the welcome dialog.

--- src/app/welcome-dialog/welcome-dialog.component.ts
+++ src/app/welcome-dialog/welcome-dialog.component.ts
@@ -1,5 +1,11 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { shell } from 'electron';
+
+import { Analytics } from '../../../src-shared/analytics/analytics';
+import { Logger } from '../../../src-shared/log/logger';
 import { ProxyRequire } from '../../../src-shared/require/proxy-require';
+import { IconDataUrl } from '../../assets/icon-data-url';
 import { configureOpeningInOsBrowser } from '../shared/configure-opening-in-os-browser';
 import { WelcomeDialogAtAppLaunchService } from './welcome-dialog-at-app-launch/welcome-dialog-at-app-launch.service';
 
@@ -13,18 +19,34 @@ const app = ProxyRequire.electron.remote.app;
 export class WelcomeDialogComponent implements AfterViewInit {
   public readonly appVersion = app.getVersion();
 
-  @ViewChild('developedByLink') public developedByLink: ElementRef<HTMLAnchorElement>;
   @ViewChild('gitHubIssuesLink') public gitHubIssuesLink: ElementRef<HTMLAnchorElement>;
 
-  constructor(private welcomeDialogAtAppLaunchService: WelcomeDialogAtAppLaunchService) {
+  public get twitterLogoDataUrl() { return this.sanitizer.bypassSecurityTrustResourceUrl(IconDataUrl.twitterLogo); }
+  public get gitHubLogoDataUrl() { return this.sanitizer.bypassSecurityTrustResourceUrl(IconDataUrl.gitHubLogo); }
+
+  constructor(private sanitizer: DomSanitizer,
+              private welcomeDialogAtAppLaunchService: WelcomeDialogAtAppLaunchService) {
   }
 
   public ngAfterViewInit() {
-    configureOpeningInOsBrowser(this.developedByLink, 'https://github.com/TomoyukiAota');
     configureOpeningInOsBrowser(this.gitHubIssuesLink, 'https://github.com/TomoyukiAota/photo-location-map/issues');
   }
 
-  onOkClicked() {
+  public handleTwitterProfileIconClicked() {
+    // noinspection JSIgnoredPromiseFromCall
+    shell.openExternal('https://twitter.com/TomoyukiAota');
+    Logger.info(`Opened Twitter Profile of Tomoyuki Aota on Welcome Dialog.`);
+    Analytics.trackEvent(`Opened Twitter Profile of Tomoyuki Aota on Welcome Dialog`, '');
+  }
+
+  public handleGitHubProfileIconClicked() {
+    // noinspection JSIgnoredPromiseFromCall
+    shell.openExternal('https://github.com/TomoyukiAota');
+    Logger.info(`Opened GitHub Profile of Tomoyuki Aota on Welcome Dialog.`);
+    Analytics.trackEvent(`Opened GitHub Profile of Tomoyuki Aota on Welcome Dialog`, '');
+  }
+
+  public onOkClicked() {
     this.welcomeDialogAtAppLaunchService.saveThatUserClickedOk();
   }
 }
