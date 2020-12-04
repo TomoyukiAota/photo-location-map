765e1c57e376eeb593c29339432814aaada9c4f5
Tomoyuki Aota <tomoyukia@gmail.com>
2020-12-04 22:30:39 +0900

    Use electron-mocha's preload option instead of mocha's hook to set window.__electronMochaRenderer__ = true;

---
+++ test/electron-mocha-renderer-preload.js
@@ -0,0 +1,1 @@
+window.__electronMochaRenderer__ = true;
--- test/electron-mocha-renderer.mocharc.js
+++ test/electron-mocha-renderer.mocharc.js
@@ -1,7 +1,7 @@
 module.exports = {
   require: [
-    './test/electron-mocha-renderer.hooks.js',
     'espower-typescript/guess'
   ],
   spec: 'test/**/*.spec.em.@(renderer|both).ts',
+  timeout: 60000,           // Increased from 2000 ms (default) to 60000 ms to avoid "Timeout of 2000ms exceeded" error in GitHub Actions.
 };
