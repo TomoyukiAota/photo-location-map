181809c9919e08e14f9794b56e40f66f43e3bd30
Tomoyuki Aota <tomoyukia@gmail.com>
2020-12-06 23:15:17 +0900

    Remove FallbackPhotoViewer.

--- src/app/photo-viewer/fallback-photo-viewer/fallback-photo-viewer-data-url.ts
+++
@@ -1,9 +0,0 @@
-import { FallbackPhotoViewerHtml } from './fallback-photo-viewer-html';
-import { Photo } from '../../shared/model/photo.model';
-
-export class FallbackPhotoViewerDataUrl {
-  public static create(photo: Photo) {
-    const html = FallbackPhotoViewerHtml.create(photo);
-    return 'data:text/html;charset=UTF-8,' + encodeURIComponent(html);
-  }
-}
