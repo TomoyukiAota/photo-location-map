92fc7b63d3f4df3bc4b3b946155716587a3a0705
Tomoyuki Aota <tomoyukia@gmail.com>
2020-12-06 23:15:17 +0900

    Remove FallbackPhotoViewer.

--- src/app/photo-info-viewer/launch-photo-viewer-icon-element.ts
+++ src/app/photo-info-viewer/launch-photo-viewer-icon-element.ts
@@ -2,7 +2,7 @@ import { Analytics } from '../../../src-shared/analytics/analytics';
 import { Logger } from '../../../src-shared/log/logger';
 import { Photo } from '../shared/model/photo.model';
 import { IconDataUrl } from '../../assets/icon-data-url';
-import { PhotoViewerLauncher } from '../photo-viewer/photo-viewer-launcher';
+import { PhotoViewerLauncher } from '../photo-viewer-launcher/photo-viewer-launcher';
 
 export class LaunchPhotoViewerIconElement {
   public static create(photo: Photo): HTMLImageElement {
