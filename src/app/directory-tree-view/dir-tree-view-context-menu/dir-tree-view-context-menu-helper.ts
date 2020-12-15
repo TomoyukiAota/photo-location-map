51d71f645b5147bc45d7351b15d98b3b5328b37a
Tomoyuki Aota <tomoyukia@gmail.com>
2020-12-16 01:29:17 +0900

    Fix the issue that the first element on the context menu is focused.

--- src/app/directory-tree-view/dir-tree-view-context-menu/dir-tree-view-context-menu-helper.ts
+++ src/app/directory-tree-view/dir-tree-view-context-menu/dir-tree-view-context-menu-helper.ts
@@ -1,7 +1,10 @@
 import { FocusMonitor } from '@angular/cdk/a11y';
 import { MatMenuTrigger } from '@angular/material/menu';
+import { createPrependedLogger } from '../../../../src-shared/log/create-prepended-logger';
 import { FlatNode } from '../directory-tree-view.model';
 import { DirTreeViewContextMenuData } from './dir-tree-view-context-menu-data';
+
+const logger = createPrependedLogger('[Directory Tree View]');
 
 export class DirTreeViewContextMenuHelper {
   public static createData(node: FlatNode): DirTreeViewContextMenuData {
@@ -14,8 +17,11 @@ export class DirTreeViewContextMenuHelper {
   // Disable focus on all buttons in order to get rid of the first button being automatically focused when the context menu is opened.
   // See https://stackoverflow.com/a/51419613/7947548
   public static disableFocus(focusMonitor: FocusMonitor): void {
-    const buttons = Array.from(document.getElementsByClassName('dir-tree-view-context-menu-button'));
+    const buttons = Array.from(document.getElementsByClassName('context-menu-button'));
     buttons.forEach((button: HTMLElement) => focusMonitor.stopMonitoring(button));
+    if (buttons.length === 0) {
+      logger.warn(`Context Menu: No buttons are found to configure disabling focus.`);
+    }
   }
 
   // Close the context menu when the overlay receives right click.
