08e10a7f6fc892067313d870a636c5a5259eb8fd
Tomoyuki Aota <tomoyukia@gmail.com>
2021-09-19 22:10:34 +0900

    Display LoadedFilesStatusBarComponent.

---
+++ src/app/loaded-files-status-bar/component/loaded-files-status-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loaded-files-status-bar',
+  templateUrl: './loaded-files-status-bar.component.html',
+  styleUrls: ['./loaded-files-status-bar.component.scss']
+})
+export class LoadedFilesStatusBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
