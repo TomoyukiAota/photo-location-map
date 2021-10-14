c96ec84b03c6dfcd80a1c0a18796ac4c49272100
Tomoyuki Aota <tomoyukia@gmail.com>
2021-10-15 02:22:44 +0900

    Display proper messages around the end of loading files.

--- src/app/loading-folder/progress-reporter/loading-folder-progress-reporter.service.ts
+++ src/app/loading-folder/progress-reporter/loading-folder-progress-reporter.service.ts
@@ -3,13 +3,19 @@ import { Subject } from 'rxjs';
 import { Logger } from '../../../../src-shared/log/logger';
 import { LoadingFolderProgress as progress } from '../../shared/loading-folder-progress';
 
+interface LoadingFolderProgressStatus {
+  numberOfLoadedFiles: number;
+  numberOfAllFilesToLoad: number;
+  loadedPercent: number;
+  isValid: boolean;
+  isCompleted: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingFolderProgressReporterService {
-  public progressStatus = new Subject<{numberOfLoadedFiles: number, numberOfAllFilesToLoad: number, loadedPercent: number, isValid: boolean}>();
-
-  constructor() { }
+  public progressStatus = new Subject<LoadingFolderProgressStatus>();
 
   public startUpdatingProgress(): void {
     const updateMilliseconds = 100;
@@ -18,15 +24,17 @@ export class LoadingFolderProgressReporterService {
       const numberOfAllFilesToLoad = progress.numberOfAllFilesToLoad;
       const loadedPercent = numberOfLoadedFiles / numberOfAllFilesToLoad * 100;
       const isValid = numberOfAllFilesToLoad !== 0;
+      const isCompleted = numberOfLoadedFiles === numberOfAllFilesToLoad;
       Logger.info(`[Loading Folder] Loaded ${numberOfLoadedFiles} files out of ${numberOfAllFilesToLoad} files.`);
       this.progressStatus.next({
         isValid,
         numberOfLoadedFiles,
         numberOfAllFilesToLoad,
         loadedPercent,
+        isCompleted,
       });
 
-      if (numberOfLoadedFiles === numberOfAllFilesToLoad) {
+      if (isCompleted) {
         clearInterval(intervalId);
       }
     }, updateMilliseconds);
