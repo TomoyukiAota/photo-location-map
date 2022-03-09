4c6cce5720f6fec61d16dc206e34476d2626866d
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-09 23:42:30 +0900

    Update to Electron 17.1.1. Update Electron-related packages.

--- test/em-renderer-require-in-main-process.js
+++ test/em-renderer-require-in-main-process.js
@@ -3,3 +3,8 @@
 // Without this file, the tests will fail.
 
 require('@electron/remote/main').initialize();
+
+// Reference: https://github.com/electron/remote/issues/94#issuecomment-1024849702
+require('electron').app.on('browser-window-created', (_, window) => {
+  require('@electron/remote/main').enable(window.webContents);
+});
