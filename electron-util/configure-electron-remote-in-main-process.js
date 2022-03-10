25d40ada26c345530d7d5a4d08126f5e15cb9c55
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-11 02:57:49 +0900

    Rename variable.

--- electron-util/configure-electron-remote-in-main-process.js
+++ electron-util/configure-electron-remote-in-main-process.js
@@ -1,6 +1,6 @@
 require('@electron/remote/main').initialize();
 
 // Reference: https://github.com/electron/remote/issues/94#issuecomment-1024849702
-require('electron').app.on('browser-window-created', (_, window) => {
-  require('@electron/remote/main').enable(window.webContents);
+require('electron').app.on('browser-window-created', (_, browserWindow) => {
+  require('@electron/remote/main').enable(browserWindow.webContents);
 });
