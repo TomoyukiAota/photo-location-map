5bb8ca90944c2c3c3d3cec8ca897ae22e31985f3
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-11 02:28:58 +0900

    Rename the folder from electron-web-preferences to electron-util.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const electronWebPreferences = require('../electron-web-preferences/electron-web-preferences');
+const electronWebPreferences = require('../electron-util/electron-web-preferences');
 
 module.exports = function (config) {
   config.set({
