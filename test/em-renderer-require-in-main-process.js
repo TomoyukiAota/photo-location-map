f2ff2c7f91c22b26848af78145584935ad5a09a2
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-11 02:50:18 +0900

    Consolidate the code to configure @electron/remote in the main process.

--- test/em-renderer-require-in-main-process.js
+++ test/em-renderer-require-in-main-process.js
@@ -1,10 +1,5 @@
-// This file is to initialize @electron/remote in the Electron main process
+// This file is to configure @electron/remote in the Electron main process
 // before running electron-mocha's tests in the renderer process.
 // Without this file, the tests will fail.
 
-require('@electron/remote/main').initialize();
-
-// Reference: https://github.com/electron/remote/issues/94#issuecomment-1024849702
-require('electron').app.on('browser-window-created', (_, window) => {
-  require('@electron/remote/main').enable(window.webContents);
-});
+require('../electron-util/configure-electron-remote-in-main-process');
