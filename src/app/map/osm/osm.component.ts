60ddb1480982f5354befc2d7c7d1192658a8ca83
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-15 01:41:27 +0900

    Display Bing Maps.

--- src/app/map/osm/osm.component.ts
+++ src/app/map/osm/osm.component.ts
@@ -6,6 +6,12 @@ import { SelectedPhotoService } from '../../shared/service/selected-photo.servic
 import { PhotoInfoViewerContent } from '../../photo-info-viewer/photo-info-viewer-content';
 import { OsmForceRenderService } from './osm-force-render/osm-force-render.service';
 
+// References to implement Bing Maps with leaflet-plugins:
+// - https://github.com/shramov/leaflet-plugins/blob/master/examples/bing.html
+// - https://github.com/shramov/leaflet-plugins/blob/master/layer/tile/Bing.js
+import 'leaflet-plugins/layer/tile/Bing'; // Equivalent to <script src="https://github.com/shramov/leaflet-plugins/blob/master/layer/tile/Bing.js"></script>
+import 'leaflet-plugins/layer/tile/Bing.addon.applyMaxNativeZoom'
+
 // "declare let L: any;" is defined here (in addition to global scope typing given by type-declaration/index.d.ts)
 // in order to avoid the compile errors for the types from leaflet.markercluster
 declare let L: any;
@@ -64,12 +70,33 @@ export class OsmComponent implements OnInit, OnDestroy, AfterViewInit {
 
   private initializeOsm(): void {
     this.map = L.map('osm').setView([0, 0], 2);
-    const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+
+    const bingMapsKey = '96S0sLgTrpX5VudevEyg~93qOp_-tPdiBcUw_Q-mpUg~AtbViWkzvmAlU9MB08o4mka92JlnRQnYHrHP8GKZBbl0caebqVS95jsvOKVHvrt3';
+    const bingMapsOptions = {
+      key: bingMapsKey,
+      maxNativeZoom: 19,
+      maxZoom: 19,
+      culture: this.getCultureForMap(),
+    };
+    const bingRoadOnDemandLayer = new L.bingLayer(L.extend({imagerySet: 'RoadOnDemand'}, bingMapsOptions));
+    const bingAerialLayer = new L.bingLayer(L.extend({imagerySet: 'Aerial'}, bingMapsOptions));
+    const bingAerialWithLabelsOnDemandLayer = new L.bingLayer(L.extend({imagerySet: 'AerialWithLabelsOnDemand'}, bingMapsOptions));
+
+    const osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
       maxNativeZoom: 19,
-      maxZoom: 19
+      maxZoom: 19,
     });
-    tileLayer.addTo(this.map);
+
+    bingRoadOnDemandLayer.addTo(this.map); // Set Bing's RoadOnDemand as the default map.
+
+    L.control.layers({
+      'Bing (Roads)': bingRoadOnDemandLayer,
+      'Bing (Aerial)': bingAerialLayer,
+      'Bing (Aerial with Labels)': bingAerialWithLabelsOnDemandLayer,
+      'OpenStreetMaps': osmLayer,
+    }).addTo(this.map);
+
     this.map.once('moveend', event => {
       const initialMaxZoomLevel = 13;
       if (this.map.getZoom() > initialMaxZoomLevel) {
@@ -89,5 +116,14 @@ export class OsmComponent implements OnInit, OnDestroy, AfterViewInit {
 
     this.map.addLayer(markerClusterGroup);
     this.map.fitBounds(markerClusterGroup.getBounds());
+  }
+
+  private getCultureForMap(): string {
+    // navigator.language is used because it seems to match the supported culture code for Bing Maps.
+    // For Bing Maps, it's still safe in case navigator.language does not match the supported culture code
+    // because passing some random string for the culture option results in the default culture (en-US).
+    // navigator.language on MDN: https://developer.mozilla.org/en-US/docs/Web/API/Navigator/language
+    // Bing Maps Supported Culture Codes: https://docs.microsoft.com/en-us/bingmaps/rest-services/common-parameters-and-types/supported-culture-codes
+    return navigator.language;
   }
 }
