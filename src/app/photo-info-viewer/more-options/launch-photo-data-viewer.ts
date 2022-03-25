6bffa93f90abcdad61f10acb8768c92e7010aab8
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-25 21:17:39 +0900

    Launch Photo Data Viewer from "View Data" menu item.

---
+++ src/app/photo-info-viewer/more-options/launch-photo-data-viewer.ts
@@ -0,0 +1,12 @@
+import { BrowserWindow } from '@electron/remote';
+import { Photo } from '../../shared/model/photo.model';
+
+export async function launchPhotoDataViewer(photo: Photo) {
+  const browserWindow = new BrowserWindow({
+    title: `View Data: ${photo.name}`,
+    autoHideMenuBar: true,
+    useContentSize: true,
+  });
+  await browserWindow.loadURL('http://localhost:3000');
+  browserWindow.show();
+}
