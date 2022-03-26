f95676c6f1666aebcb8877ec9be5260399c57a61
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-27 06:41:53 +0900

    Prepare sever URL strings depending on deployment environment.

--- src/app/photo-info-viewer/more-options/launch-photo-data-viewer.ts
+++ src/app/photo-info-viewer/more-options/launch-photo-data-viewer.ts
@@ -1,12 +1,21 @@
 import { BrowserWindow } from '@electron/remote';
 import { Photo } from '../../shared/model/photo.model';
 
+// Production Server URL
+// const urlStr = 'https://photo-data-viewer-plm.vercel.app/';
+
+// Production Preview Server URL
+// const urlStr = 'https://photo-data-viewer-git-feature-photo-locatio-d5bc15-tomoyukiaota.vercel.app/';
+
+// Development Server URL
+const urlStr = 'http://localhost:3000';
+
 export async function launchPhotoDataViewer(photo: Photo) {
   const searchParamsObj = {
     photoPath: photo.path,
   };
 
-  const photoDataViewerUrl = new URL('http://localhost:3000');
+  const photoDataViewerUrl = new URL(urlStr);
   photoDataViewerUrl.search = new URLSearchParams(searchParamsObj).toString();
 
   const browserWindow = new BrowserWindow({
