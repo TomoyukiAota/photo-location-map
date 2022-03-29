7cb8183341438231a0e4aae30155abb7e12d9ec9
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-30 01:54:51 +0900

    Disable hardware acceleration.

--- src-main/electron-main.ts
+++ src-main/electron-main.ts
@@ -67,6 +67,10 @@ const createWindow = () => {
 
 
 try {
+  // Disable hardware acceleration to address the error printed
+  // as "Passthrough is not supported, GL is disabled, ANGLE is".
+  app.disableHardwareAcceleration();
+
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
