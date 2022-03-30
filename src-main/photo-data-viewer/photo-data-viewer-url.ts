27bf4867d73109f8783611037d64fa52e1b15a1f
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-30 22:08:01 +0900

    Extract the server config into a dedicated file.

--- src-main/photo-data-viewer/photo-data-viewer-url.ts
+++ src-main/photo-data-viewer/photo-data-viewer-url.ts
@@ -1,23 +1,12 @@
 import { PhotoDataViewerIpcPhotoParams } from '../../src-shared/photo-data-viewer/photo-data-viewer-ipc-params';
+import { getPhotoDataViewerServerUrl } from './photo-data-viewer-server-config';
 
-function getServerUrlStr(): string {
-  // Production Server URL
-  // const serverUrlStr = 'https://photo-data-viewer-plm.vercel.app/';
-
-  // Production Preview Server URL
-  const serverUrlStr = 'https://photo-data-viewer-git-feature-photo-locatio-d5bc15-tomoyukiaota.vercel.app/';
-
-  // Development Server URL
-  // const serverUrlStr = 'http://localhost:3000';
-
-  return serverUrlStr;
-}
-
-export function getPhotoDataViewerUrl(photo: PhotoDataViewerIpcPhotoParams): string {
+export function createPhotoDataViewerUrl(photo: PhotoDataViewerIpcPhotoParams): string {
   const searchParamsObj = {
     photoPath: photo.path,
   };
-  const photoDataViewerUrl = new URL(getServerUrlStr());
+  const serverUrl = getPhotoDataViewerServerUrl();
+  const photoDataViewerUrl = new URL(serverUrl);
   photoDataViewerUrl.search = new URLSearchParams(searchParamsObj).toString();
   return photoDataViewerUrl.href;
 }
