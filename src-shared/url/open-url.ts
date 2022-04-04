d197138c516ea82a4f1416f7d74492b555fa434d
Tomoyuki Aota <tomoyukia@gmail.com>
2022-04-05 01:10:06 +0900

    Move openUrl function. Use openUrl function for URLs in Photo Data Viewer.

---
+++ src-shared/url/open-url.ts
@@ -0,0 +1,10 @@
+import { shell } from 'electron';
+import { Analytics } from '../analytics/analytics';
+import { Logger } from '../log/logger';
+
+export function openUrl(url: string, urlDescription: string, from: string, urlForAnalytics = url): void {
+  // noinspection JSIgnoredPromiseFromCall
+  shell.openExternal(url);
+  Logger.info(`[${from}] Opened URL for ${urlDescription} - ${url}`);
+  Analytics.trackEvent(`Opened URL`, `[${from}] Opened URL for ${urlDescription}`, urlForAnalytics);
+}
