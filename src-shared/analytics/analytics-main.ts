504917a929ec3a3953b64a1129e6a7918fb965db
Tomoyuki Aota <tomoyukia@gmail.com>
2022-09-16 02:15:35 +0900

    Update logging for Google Analytics 4 and Universal Analytics.

--- src-shared/analytics/analytics-main.ts
+++ src-shared/analytics/analytics-main.ts
@@ -11,14 +11,14 @@ export class AnalyticsMain implements AnalyticsInterface {
     const userId = AnalyticsConfig.userId;
     const ua = require('universal-analytics');
     this.visitor = ua(trackingId, userId);
-    Logger.info(`[Google Analytics] Universal Analytics Tracking ID: ${trackingId}`);
-    Logger.info(`[Google Analytics] User ID: ${userId}`);
+    Logger.info(`[Universal Analytics] Tracking ID: ${trackingId}`);
+    Logger.info(`[Universal Analytics] User ID: ${userId}`);
   }
 
   public setUserAgent(userAgent: string) {
     this.userAgent = userAgent;
     this.visitor.set('userAgentOverride', this.userAgent);
-    Logger.info(`[Google Analytics] User Agent for Google Analytics is "${this.userAgent}"`);
+    Logger.info(`[Universal Analytics] User Agent for Universal Analytics is "${this.userAgent}"`);
   }
 
   public trackEvent(category: string, action: string, label?: string, value?: string | number): void {
--- src-shared/analytics/initialize-gtag.ts
+++ src-shared/analytics/initialize-gtag.ts
@@ -1,3 +1,4 @@
+import { Logger } from '../log/logger';
 import { AnalyticsConfig } from './analytics-config';
 
 export function initializeGtag() {
@@ -5,4 +6,6 @@ export function initializeGtag() {
   gtag('config', AnalyticsConfig.googleAnalytics4MeasurementId, {
     'user_id': AnalyticsConfig.userId,
   });
+  Logger.info(`[Google Analytics 4] Measurement ID: ${AnalyticsConfig.googleAnalytics4MeasurementId}`);
+  Logger.info(`[Google Analytics 4] User ID: ${AnalyticsConfig.userId}`);
 }
