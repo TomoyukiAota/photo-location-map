e62e060722dc670f16c22dd40feb60ff34ab7054
Tomoyuki Aota <tomoyukia@gmail.com>
2022-09-18 17:43:38 +0900

    Rename to GtagWrapper.

--- src-shared/analytics/analytics-renderer.ts
+++ src-shared/analytics/analytics-renderer.ts
@@ -1,11 +1,11 @@
 import { AnalyticsInterface } from './analytics-interface';
 import { ProxyRequire } from '../require/proxy-require';
 import { AnalyticsIpcChannelName } from './analytics-ipc';
-import { GoogleAnalytics4Helper } from './google-analytics-4-helper';
+import { GtagWrapper } from './gtag-wrapper';
 
 export class AnalyticsRenderer implements AnalyticsInterface {
   trackEvent(category: string, action: string, label?: string, value?: string | number): void {
-    GoogleAnalytics4Helper.trackEvent(category, action, label, value);
+    GtagWrapper.trackEvent(category, action, label, value);
 
     // Send an event to Universal Analytics
     ProxyRequire.electron.ipcRenderer.send(
