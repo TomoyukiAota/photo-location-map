66db39b9d70c4347e3dd136c2fa47dd24884115a
Tomoyuki Aota <tomoyukia@gmail.com>
2022-09-23 19:02:16 +0900

    Check if GtagWrapper::initialize is called before calling GtagWrapper::trackEvent.

--- src-shared/analytics/library-wrapper/gtag-wrapper.ts
+++ src-shared/analytics/library-wrapper/gtag-wrapper.ts
@@ -3,6 +3,8 @@ import { AnalyticsConfig } from '../config/analytics-config';
 import { GoogleAnalytics4Config } from '../config/google-analytics-4-config';
 
 export class GtagWrapper {
+  private static isInitialized = false;
+
   public static initialize() {
     const measurementId = GoogleAnalytics4Config.measurementId;
     const userId = AnalyticsConfig.userId;
@@ -12,9 +14,15 @@ export class GtagWrapper {
     });
     Logger.info(`[Google Analytics 4] Measurement ID: ${measurementId}`);
     Logger.info(`[Google Analytics 4] User ID: ${userId}`);
+    this.isInitialized = true;
   }
 
   public static trackEvent(category: string, action: string, label?: string, value?: string | number) {
+    if (!this.isInitialized) {
+      Logger.error('GtagWrapper::initialize needs to be called before calling GtagWrapper::trackEvent');
+      return;
+    }
+
     const eventName = action;
     if (eventName.length > 40) {
       Logger.warn(`[Google Analytics 4] Event name length exceeds the limit (40 characters).`);
