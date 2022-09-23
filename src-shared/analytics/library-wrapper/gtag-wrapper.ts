33dbf56fdfd2664f36feaaec40437380a973831c
Tomoyuki Aota <tomoyukia@gmail.com>
2022-09-23 19:29:53 +0900

    Update logging and initialization check for analytics.

--- src-shared/analytics/library-wrapper/gtag-wrapper.ts
+++ src-shared/analytics/library-wrapper/gtag-wrapper.ts
@@ -1,6 +1,8 @@
-import { Logger } from '../../log/logger';
+import { createPrependedLogger } from '../../log/create-prepended-logger';
 import { AnalyticsConfig } from '../config/analytics-config';
 import { GoogleAnalytics4Config } from '../config/google-analytics-4-config';
+
+const ga4Logger = createPrependedLogger('[Google Analytics 4]');
 
 export class GtagWrapper {
   private static isInitialized = false;
@@ -8,25 +10,25 @@ export class GtagWrapper {
   public static initialize() {
     const measurementId = GoogleAnalytics4Config.measurementId;
     const userId = AnalyticsConfig.userId;
+    ga4Logger.info(`Measurement ID: ${measurementId}`);
+    ga4Logger.info(`User ID: ${userId}`);
     gtag('js', new Date());
     gtag('config', measurementId, {
       'user_id': userId,
     });
-    Logger.info(`[Google Analytics 4] Measurement ID: ${measurementId}`);
-    Logger.info(`[Google Analytics 4] User ID: ${userId}`);
     this.isInitialized = true;
   }
 
   public static trackEvent(category: string, action: string, label?: string, value?: string | number) {
     if (!this.isInitialized) {
-      Logger.error('GtagWrapper::initialize needs to be called before calling GtagWrapper::trackEvent');
+      ga4Logger.error('GtagWrapper::initialize needs to be called before calling GtagWrapper::trackEvent');
       return;
     }
 
     const eventName = action;
     if (eventName.length > 40) {
-      Logger.warn(`[Google Analytics 4] Event name length exceeds the limit (40 characters).`);
-      Logger.warn(`[Google Analytics 4] event name (action): ${eventName}, category: ${category}, label: ${label}, value: ${value}`);
+      ga4Logger.warn(`Event name length exceeds the limit (40 characters).`);
+      ga4Logger.warn(`event name (action): ${eventName}, category: ${category}, label: ${label}, value: ${value}`);
     }
 
     gtag('event', eventName, {
