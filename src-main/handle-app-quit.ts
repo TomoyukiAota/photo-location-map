46f7cc19afe9e8b67e32ce9ef8be77e1d67cf3c5
Tomoyuki Aota <tomoyukia@gmail.com>
2022-10-09 00:45:25 +0900

    Mention the cache folder location on Windows for the session's HTTP cache.

--- src-main/handle-app-quit.ts
+++ src-main/handle-app-quit.ts
@@ -6,6 +6,7 @@ export function handleAppQuit() {
 
   // Clear the session's HTTP cache when the app quits so that the cache folder size stays small.
   // An example of the cache is the map because the cache files are created after zooming in/out the map.
+  // Cache folder location on Windows: C:\Users\%USERNAME%\AppData\Roaming\Photo Location Map\Cache
   // ------------------------------------------------------
   // Before clearing it for the first time on October 2022,
   // the cache folder size was 434 MB, and the oldest file in the cache folder exists from March 2022.
