e8ba3de24185fc9856cc0cbd50804256a415281a
Tomoyuki Aota <tomoyukia@gmail.com>
2022-10-08 17:14:21 +0900

    Clear the V8 generated JS code cache for the session when the app quits.

--- src-main/handle-app-quit.ts
+++ src-main/handle-app-quit.ts
@@ -11,5 +11,16 @@ export function handleAppQuit() {
   // Before clearing it for the first time on October 2022,
   // the cache folder size was 434 MB, and the oldest file in the cache folder exists from March 2022.
   // It seems like some portion of the cache is no longer used but still exists.
+  // After calling clearCache, only few files remain and the folder size decreases to 800 kB.
   session.defaultSession.clearCache();
+
+  // Clear the V8 generated JS code cache for the session when the app quits so that the cache folder size stays small.
+  // Cache folder location on Windows: C:\Users\%USERNAME%\AppData\Roaming\Photo Location Map\Code Cache
+  // ------------------------------------------------------
+  // Before clearing it for the first time on October 2022,
+  // the cache folder size was 288 MB, and the oldest file in the cache folder exists from August 2019.
+  // It seems like some portion of the cache is no longer used but still exists.
+  // Calling clearCodeCaches deletes not all but only some files (e.g. the folder size decreases from 288 MB to 237 MB),
+  // and old files still remain, but calling clearCodeCaches is implemented as an effort to keep the folder size small.
+  session.defaultSession.clearCodeCaches({urls: []});
 }
