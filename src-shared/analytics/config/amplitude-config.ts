677d25621de8c72070fe8137a2391f06c2c563f5
Tomoyuki Aota <tomoyukia@gmail.com>
2022-10-11 02:35:24 +0900

    Configure the API key for the prod project for Amplitude.

--- src-shared/analytics/config/amplitude-config.ts
+++ src-shared/analytics/config/amplitude-config.ts
@@ -3,7 +3,7 @@ import { isPrereleaseVersion } from '../../version/is-prerelease-version';
 
 class AmplitudeApiKey {
   public static dev  = '504cfac385ea79239a18052ee430fc19';
-  public static prod = 'TODO';
+  public static prod = '302caa58852ce254c232fb74d27ddbd0';
 }
 
 export class AmplitudeConfig {
