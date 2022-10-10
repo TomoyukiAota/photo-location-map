3205e24ec7a09ef539ff8a498c2727d5a77ea067
Tomoyuki Aota <tomoyukia@gmail.com>
2022-10-11 02:54:44 +0900

    Configure the project token for the prod project for Mixpanel.

--- src-shared/analytics/config/mixpanel-config.ts
+++ src-shared/analytics/config/mixpanel-config.ts
@@ -3,7 +3,7 @@ import { isPrereleaseVersion } from '../../version/is-prerelease-version';
 
 class MixpanelProjectToken {
   public static dev  = '9f48c4c504ae32885d31d8c0910d072a';
-  public static prod = 'TODO';
+  public static prod = '4b495f033948124840cad7a1e72a0d3c';
 }
 
 export class MixpanelConfig {
