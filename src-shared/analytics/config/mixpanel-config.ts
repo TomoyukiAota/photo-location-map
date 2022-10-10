330ae267dd4aa3bb492372ca59f37eced2bebf85
Tomoyuki Aota <tomoyukia@gmail.com>
2022-10-11 02:50:40 +0900

    Switch from the old dev project to the new dev project for Mixpanel in order to reset the data.

--- src-shared/analytics/config/mixpanel-config.ts
+++ src-shared/analytics/config/mixpanel-config.ts
@@ -2,7 +2,7 @@ import { DevOrProd } from '../../dev-or-prod/dev-or-prod';
 import { isPrereleaseVersion } from '../../version/is-prerelease-version';
 
 class MixpanelProjectToken {
-  public static dev  = '269bea2bd2dc884b1ec8fac9174acdc4';
+  public static dev  = '9f48c4c504ae32885d31d8c0910d072a';
   public static prod = 'TODO';
 }
 
