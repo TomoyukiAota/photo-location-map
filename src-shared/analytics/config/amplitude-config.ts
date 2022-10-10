19b22266f4b12118047853ff8fad93b3dc0a1174
Tomoyuki Aota <tomoyukia@gmail.com>
2022-10-11 02:29:08 +0900

    Switch from the old dev project to the new dev project for Amplitude in order to reset the data.

--- src-shared/analytics/config/amplitude-config.ts
+++ src-shared/analytics/config/amplitude-config.ts
@@ -2,7 +2,7 @@ import { DevOrProd } from '../../dev-or-prod/dev-or-prod';
 import { isPrereleaseVersion } from '../../version/is-prerelease-version';
 
 class AmplitudeApiKey {
-  public static dev = '62cae04529b44c7ea829e49a64d0e199';
+  public static dev = '504cfac385ea79239a18052ee430fc19';
   public static prod = 'TODO';
 }
 
