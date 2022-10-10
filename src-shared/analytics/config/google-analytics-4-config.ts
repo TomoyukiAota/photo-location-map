3c17751196adaa194602edcc063572a9455e49e0
Tomoyuki Aota <tomoyukia@gmail.com>
2022-10-11 02:33:44 +0900

    Align code.

--- src-shared/analytics/config/google-analytics-4-config.ts
+++ src-shared/analytics/config/google-analytics-4-config.ts
@@ -2,7 +2,7 @@ import { DevOrProd } from '../../dev-or-prod/dev-or-prod';
 import { isPrereleaseVersion } from '../../version/is-prerelease-version';
 
 class GoogleAnalytics4MeasurementId {
-  public static dev = 'G-FWHPKTH800';
+  public static dev  = 'G-FWHPKTH800';
   public static prod = 'G-5KPL04TELK';
 }
 
