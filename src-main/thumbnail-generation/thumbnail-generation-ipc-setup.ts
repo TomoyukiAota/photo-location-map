528ac43815f6aa94c39928b8a8395edc02812e7e
Tomoyuki Aota <tomoyukia@gmail.com>
2022-12-08 22:22:54 +0900

    Use thumbnailGenerationLogger.

--- src-main/thumbnail-generation/thumbnail-generation-ipc-setup.ts
+++ src-main/thumbnail-generation/thumbnail-generation-ipc-setup.ts
@@ -1,9 +1,9 @@
 import { ipcMain } from 'electron';
 import { IpcConstants } from '../../src-shared/ipc/ipc-constants';
-import { Logger } from '../../src-shared/log/logger';
+import { thumbnailGenerationLogger as logger } from '../../src-shared/thumbnail/generation/thumbnail-generation-logger';
 import { handleThumbnailGenerationIpcRequest } from './thumbnail-generation';
 
 ipcMain.handle(IpcConstants.ThumbnailGenerationInMainProcess.Name, (event, allHeifFilePaths: string[], heifFilePathsToGenerateThumbnail: string[]) => {
-  Logger.info(`Received the IPC invoke request about thumbnail generation in the main process.`);
+  logger.info(`Received the IPC invoke request about thumbnail generation in the main process.`);
   handleThumbnailGenerationIpcRequest(allHeifFilePaths, heifFilePathsToGenerateThumbnail);
 });
