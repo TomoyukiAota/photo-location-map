541da978c8827427f5c2a37fddd4d8288d43cd62
Tomoyuki Aota <tomoyukia@gmail.com>
2023-01-03 16:45:13 +0900

    Stop displaying the size of the thumbnail cache folder due to the issue that the settings dialog becomes sluggish when the cache becomes big.

--- src/app/settings-dialog/cache-settings/cache-settings.component.ts
+++ src/app/settings-dialog/cache-settings/cache-settings.component.ts
@@ -1,8 +1,7 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as fsExtra from 'fs-extra';
 import { openWithAssociatedApp } from '../../../../src-shared/command/command';
 import { Logger } from '../../../../src-shared/log/logger';
-import { getSizeInStringFormat } from '../../../../src-shared/plm-fs-util/plm-fs-util';
 import { RequireFromMainProcess } from '../../../../src-shared/require/require-from-main-process';
 import { plmThumbnailCacheDir } from '../../../../src-shared/thumbnail/cache/thumbnail-cache-util';
 import { configureOpeningInOsBrowser } from '../../shared/open-url/configure-opening-in-os-browser';
@@ -12,34 +11,18 @@ import { configureOpeningInOsBrowser } from '../../shared/open-url/configure-ope
   templateUrl: './cache-settings.component.html',
   styleUrls: ['./cache-settings.component.scss']
 })
-export class CacheSettingsComponent implements AfterViewInit, OnDestroy, OnInit {
+export class CacheSettingsComponent implements AfterViewInit, OnInit {
   public thumbnailCacheLocation = plmThumbnailCacheDir;
-  public thumbnailCacheSize: string;
-  public updateThumbnailCacheSizeIntervalId: NodeJS.Timeout;
 
   @ViewChild('heifWikipediaLink') public heifWikipediaLink: ElementRef<HTMLAnchorElement>;
 
   ngOnInit(): void {
     fsExtra.ensureDirSync(this.thumbnailCacheLocation);
-    const { size, errors } = getSizeInStringFormat(this.thumbnailCacheLocation);
-    this.thumbnailCacheSize = size;
-    if (errors.length) {
-      Logger.warn(`Error(s) detected during getting the size of "${this.thumbnailCacheLocation}"`);
-    }
-    errors.forEach(error => Logger.warn(`Detected Error: ${error}`, error));
-
-    this.updateThumbnailCacheSizeIntervalId = setInterval(() => {
-      this.thumbnailCacheSize =  getSizeInStringFormat(this.thumbnailCacheLocation).size;
-    }, 1000);
   }
 
   ngAfterViewInit() {
     configureOpeningInOsBrowser(this.heifWikipediaLink, 'https://en.wikipedia.org/wiki/High_Efficiency_Image_File_Format',
                                 'HEIF page in Wikipedia', 'Settings Dialog] [Cache');
-  }
-
-  ngOnDestroy(): void {
-    clearInterval(this.updateThumbnailCacheSizeIntervalId);
   }
 
   public openThumbnailCacheLocation(): void {
