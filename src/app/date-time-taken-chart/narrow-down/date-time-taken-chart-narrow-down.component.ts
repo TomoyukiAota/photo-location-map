132eab457a947bdeff438af3675a9e2e05a5d3b6
Tomoyuki Aota <tomoyukia@gmail.com>
2023-09-18 22:57:49 +0900

    Log and track about clicking "Narrow Down" button.

--- src/app/date-time-taken-chart/narrow-down/date-time-taken-chart-narrow-down.component.ts
+++ src/app/date-time-taken-chart/narrow-down/date-time-taken-chart-narrow-down.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Analytics } from '../../../../src-shared/analytics/analytics';
 import { DirectoryTreeViewSelectionService } from '../../directory-tree-view/directory-tree-view-selection.service';
 import { PinnedPhotoService } from '../../shared/service/pinned-photo.service';
+import { dateTimeTakenChartLogger as logger } from '../date-time-taken-chart-logger';
 
 @Component({
   selector: 'app-date-time-taken-chart-narrow-down',
@@ -13,6 +15,8 @@ export class DateTimeTakenChartNarrowDownComponent {
   }
 
   public onNarrowDownClicked() {
+    logger.info('Clicked "Narrow Down" button.');
+    Analytics.trackEvent('DTT Chart', `[DTT Chart] Clicked "Narrow Down"`);
     const pinnedPhotos = this.pinnedPhotoService.getPinnedPhotos();
     const photoPaths = pinnedPhotos.map(photo => photo.path);
     this.directoryTreeViewSelectionService.select(photoPaths);
