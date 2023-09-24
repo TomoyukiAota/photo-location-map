a93ddf6fc84b74e379df10ed00dd8fee76b85da4
Tomoyuki Aota <tomoyukia@gmail.com>
2023-09-25 00:41:01 +0900

    Skip updating pinned photos when the desired pinned photos are the same as the current pinned photos.

--- src/app/shared/service/pinned-photo.service.ts
+++ src/app/shared/service/pinned-photo.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
 import { BehaviorSubject } from 'rxjs';
 import { Photo } from '../model/photo.model';
 
@@ -8,8 +9,12 @@ import { Photo } from '../model/photo.model';
 export class PinnedPhotoService {
   public pinnedPhotos = new BehaviorSubject<Photo[]>([]);    // Represents the photos pinned on the map
 
-  public setPinnedPhotos(pinnedPhotos: Photo[]) {
-    this.pinnedPhotos.next(pinnedPhotos);
+  public setPinnedPhotos(desiredPinnedPhotos: Photo[]) {
+    const desiredPinnedPhotoPaths = desiredPinnedPhotos.map(photo => photo.path);
+    const currentPinnedPhotoPaths = this.getPinnedPhotos().map(photo => photo.path);
+    const isDesiredSameAsCurrent = _.isEqual(desiredPinnedPhotoPaths.sort(), currentPinnedPhotoPaths.sort());
+    if (isDesiredSameAsCurrent) { return; } // If same, skip updating pinned photos to avoid 1) resetting the map for usability and 2) consuming the quota of the map.
+    this.pinnedPhotos.next(desiredPinnedPhotos);
   }
 
   public getPinnedPhotos(): Photo[] {
