78ce581aa1eb456cca4376b6eae0875c9efeb211
Tomoyuki Aota <tomoyukia@gmail.com>
2024-10-03 04:22:53 +0900

    Implement the sidebar with the splitter.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,12 @@
-import { Component, ChangeDetectionStrategy, AfterViewInit, OnInit, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  AfterViewInit,
+  OnInit,
+  ChangeDetectorRef,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import Split from 'split.js';
 import { LeafletMapForceRenderService } from '../map/leaflet-map/leaflet-map-force-render/leaflet-map-force-render.service';
 import { OpenFolderService } from '../shared/service/open-folder.service';
@@ -15,6 +23,8 @@ import { ThumbnailGenerationStatusBarService } from '../thumbnail-generation/sta
 export class HomeComponent implements AfterViewInit, OnInit {
   public thumbnailGenerationStatusBarVisible = false;
   public loadedFilesStatusBarVisible: boolean;
+
+  @ViewChild('sidebar') private sidebarDiv: ElementRef;
 
   constructor(private changeDetectorRef: ChangeDetectorRef,
               private openFolderService: OpenFolderService,
@@ -39,22 +49,46 @@ export class HomeComponent implements AfterViewInit, OnInit {
   }
 
   ngAfterViewInit() {
+    const gutterSize = 8; // 8px, which is the same width as the splitter gutter in Photo Data Viewer.
+
     Split(['#home-left-sidebar', '#home-right'], {
       sizes: [25, 75],
       minSize: 200,
-      gutterSize: 8, // 8px, which is the same width as the splitter gutter in Photo Data Viewer.
+      gutterSize: gutterSize,
       snapOffset: 0,
     });
+
+    this.configureSplitWithinSidebar(gutterSize);
 
     Split(['#home-map', '#home-chart'], {
       direction: 'vertical',
       sizes: [70, 30],
       minSize: 0,
-      gutterSize: 8, // 8px, which is the same width as the splitter gutter in Photo Data Viewer.
+      gutterSize: gutterSize,
       snapOffset: 0,
     });
 
     // Leaflet needs to be rendered again after Split.js starts working. Otherwise, the map is not rendered correctly.
     this.leafletMapForceRenderService.forceRenderMapWithoutPhoto();
+  }
+
+  private configureSplitWithinSidebar(gutterSize: number) {
+    const sidebarRect: DOMRect = this.sidebarDiv.nativeElement.getBoundingClientRect();
+    const sidebarHeightPx = sidebarRect.height;
+    const upperPaneMaxHeightPx = 66;
+
+    // Taking gutter into account so that the actual initial height rendered on the browser becomes the same as the max height.
+    const upperPaneInitialHeightPx = upperPaneMaxHeightPx + (gutterSize / 2);
+
+    const upperPaneInitialHeightPercent = (upperPaneInitialHeightPx / sidebarHeightPx) * 100;
+    const lowerPaneInitialHeightPercent = 100 - upperPaneInitialHeightPercent;
+    Split(['#home-sidebar-upper-pane', '#home-sidebar-lower-pane'], {
+      direction: 'vertical',
+      sizes: [upperPaneInitialHeightPercent, lowerPaneInitialHeightPercent],
+      minSize: [0, 0],
+      maxSize: [upperPaneMaxHeightPx, Infinity],
+      gutterSize: gutterSize,
+      snapOffset: 0,
+    });
   }
 }
