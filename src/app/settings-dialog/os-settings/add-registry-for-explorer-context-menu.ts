f2456f699a3066499477630f2e9b6fcebee3aa5d
Tomoyuki Aota <tomoyukia@gmail.com>
2024-11-16 03:20:47 +0900

    Update comment.

--- src/app/settings-dialog/os-settings/add-registry-for-explorer-context-menu.ts
+++ src/app/settings-dialog/os-settings/add-registry-for-explorer-context-menu.ts
@@ -3,17 +3,17 @@ import { Registry } from 'rage-edit';
 const photoLocationMapPath = String.raw`C:\Users\Tomoyuki\AppData\Local\Programs\Photo Location Map\Photo Location Map.exe`;
 
 export async function addRegistryForExplorerContextMenu() {
-  // Add context menu item for files
+  // Add the Explorer's context menu item for files
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\*\\shell\\OpenWithPhotoLocationMap', '', 'Open with &Photo Location Map');
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\*\\shell\\OpenWithPhotoLocationMap', 'icon', photoLocationMapPath);
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\*\\shell\\OpenWithPhotoLocationMap\\command', '', `${photoLocationMapPath} "%1"`);
 
-  // Add context menu item for directories
+  // Add the Explorer's context menu item for directories
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap', '', 'Open with &Photo Location Map');
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap', 'icon', photoLocationMapPath);
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap\\command', '', `${photoLocationMapPath} "%1"`);
 
-  // Add context menu item for background
+  // Add the Explorer's context menu item for the background
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\Background\\shell\\OpenWithPhotoLocationMap', '', 'Open with &Photo Location Map');
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\Background\\shell\\OpenWithPhotoLocationMap', 'icon', photoLocationMapPath);
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\Background\\shell\\OpenWithPhotoLocationMap\\command', '', `${photoLocationMapPath} "%V"`);
--- src/app/settings-dialog/os-settings/delete-registry-for-explorer-context-menu.ts
+++ src/app/settings-dialog/os-settings/delete-registry-for-explorer-context-menu.ts
@@ -1,13 +1,13 @@
 import { Registry } from 'rage-edit';
 
 export async function deleteRegistryForExplorerContextMenu() {
-  // Remove the context menu item for files
+  // Remove the Explorer's context menu item for files
   await Registry.delete('HKEY_CURRENT_USER\\Software\\Classes\\*\\shell\\OpenWithPhotoLocationMap');
 
-  // Remove the context menu item for directories
+  // Remove the Explorer's context menu item for directories
   await Registry.delete('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap');
 
-  // Remove the context menu item for the background
+  // Remove the Explorer's context menu item for the background
   await Registry.delete('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\Background\\shell\\OpenWithPhotoLocationMap');
 
   // TODO: Add logging and analytics
