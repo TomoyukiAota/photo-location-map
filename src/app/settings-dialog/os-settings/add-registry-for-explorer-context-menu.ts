07c1345c90fcbb5751be23f9c977dd5e204e0c1b
Tomoyuki Aota <tomoyukia@gmail.com>
2024-11-16 03:39:54 +0900

    Get the exe path.

--- src/app/settings-dialog/os-settings/add-registry-for-explorer-context-menu.ts
+++ src/app/settings-dialog/os-settings/add-registry-for-explorer-context-menu.ts
@@ -1,22 +1,23 @@
+import { app } from '@electron/remote';
 import { Registry } from 'rage-edit';
 
-const photoLocationMapPath = String.raw`C:\Users\Tomoyuki\AppData\Local\Programs\Photo Location Map\Photo Location Map.exe`;
+const photoLocationMapExePath = app.getPath('exe'); // e.g. 'C:\Users\Tomoyuki\AppData\Local\Programs\Photo Location Map\Photo Location Map.exe'
 
 export async function addRegistryForExplorerContextMenu() {
   // Add the Explorer's context menu item for files
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\*\\shell\\OpenWithPhotoLocationMap', '', 'Open with &Photo Location Map');
-  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\*\\shell\\OpenWithPhotoLocationMap', 'icon', photoLocationMapPath);
-  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\*\\shell\\OpenWithPhotoLocationMap\\command', '', `${photoLocationMapPath} "%1"`);
+  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\*\\shell\\OpenWithPhotoLocationMap', 'icon', photoLocationMapExePath);
+  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\*\\shell\\OpenWithPhotoLocationMap\\command', '', `${photoLocationMapExePath} "%1"`);
 
   // Add the Explorer's context menu item for directories
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap', '', 'Open with &Photo Location Map');
-  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap', 'icon', photoLocationMapPath);
-  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap\\command', '', `${photoLocationMapPath} "%1"`);
+  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap', 'icon', photoLocationMapExePath);
+  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap\\command', '', `${photoLocationMapExePath} "%1"`);
 
   // Add the Explorer's context menu item for the background
   await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\Background\\shell\\OpenWithPhotoLocationMap', '', 'Open with &Photo Location Map');
-  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\Background\\shell\\OpenWithPhotoLocationMap', 'icon', photoLocationMapPath);
-  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\Background\\shell\\OpenWithPhotoLocationMap\\command', '', `${photoLocationMapPath} "%V"`);
+  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\Background\\shell\\OpenWithPhotoLocationMap', 'icon', photoLocationMapExePath);
+  await Registry.set('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\Background\\shell\\OpenWithPhotoLocationMap\\command', '', `${photoLocationMapExePath} "%V"`);
 
   // TODO: Add logging and analytics
 }
