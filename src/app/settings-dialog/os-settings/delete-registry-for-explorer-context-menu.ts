18eb1f538fd0d5f43c6cde785ba52dbb85da2691
Tomoyuki Aota <tomoyukia@gmail.com>
2024-11-16 03:09:15 +0900

    Implement adding/deleting registry for Explorer's context menu for directories.

--- src/app/settings-dialog/os-settings/delete-registry-for-explorer-context-menu.ts
+++ src/app/settings-dialog/os-settings/delete-registry-for-explorer-context-menu.ts
@@ -5,8 +5,7 @@ export async function deleteRegistryForExplorerContextMenu() {
   await Registry.delete('HKEY_CURRENT_USER\\Software\\Classes\\*\\shell\\OpenWithPhotoLocationMap');
 
   // Remove the context menu item for directories
-  // await Registry.delete('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap');
-  // await Registry.delete('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap\\command');
+  await Registry.delete('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\shell\\OpenWithPhotoLocationMap');
 
   // Remove the context menu item for the background
   // await Registry.delete('HKEY_CURRENT_USER\\Software\\Classes\\Directory\\Background\\shell\\OpenWithPhotoLocationMap');
