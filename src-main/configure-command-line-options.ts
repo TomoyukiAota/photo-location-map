e421daebe589a50db36bfa6c427e0bada37eed63
Tomoyuki Aota <tomoyukia@gmail.com>
2024-11-18 02:18:01 +0900

    Update the description of command line options.

--- src-main/configure-command-line-options.ts
+++ src-main/configure-command-line-options.ts
@@ -3,10 +3,8 @@ import { Logger } from '../src-shared/log/logger';
 
 const program = new Command();
 program
-  .addOption(new Option('--serve', 'Launch the app in live-reload mode for development.').hideHelp())
-  .option('--open <path>',
-    'When a folder is specified, the folder is opened after the app is launched. ' +
-    'When a file is specified, the containing folder is opened after the app is launched.')
+  .addOption(new Option('--serve', 'launch the app in live-reload mode for development').hideHelp())
+  .option('--open <path>', 'open the specified file or folder after the app is launched')
   .parse(process.argv, { from: 'electron' });
 
 Logger.info('process.argv', process.argv);
