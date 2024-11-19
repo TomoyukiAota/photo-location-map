72e0d2cc53231691985a07b83ffbfdeca535e296
Tomoyuki Aota <tomoyukia@gmail.com>
2024-11-20 02:18:11 +0900

    Implemented the mechanism of opening the parent folder and selecting the specified file when a file is specified by Explorer's "Open with Photo Location Map" context menu.

--- src/app/shared/service/open-after-launch.service.ts
+++ src/app/shared/service/open-after-launch.service.ts
@@ -1,21 +1,43 @@
+import * as fsExtra from 'fs-extra';
+import * as pathModule from 'path';
 import { Injectable } from '@angular/core';
 import { ipcRenderer } from 'electron';
 import { CommandLineOptions } from '../../../../src-shared/command-line-options/command-line-options';
 import { IpcConstants } from '../../../../src-shared/ipc/ipc-constants';
+import { Logger } from '../../../../src-shared/log/logger';
+import { DirectoryTreeViewSelectionService } from '../../directory-tree-view/directory-tree-view-selection.service';
 import { OpenPathService } from './open-path.service';
+import { PhotoDataService } from './photo-data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenAfterLaunchService {
-  constructor(private openPathService: OpenPathService) { }
+  constructor(private directoryTreeViewSelectionService: DirectoryTreeViewSelectionService,
+              private openPathService: OpenPathService,
+              private photoDataService: PhotoDataService,
+  ) { }
 
   public async openAfterLaunchIfNeeded() {
     const commandLineOptions = await this.getCommandLineOptionsFromMainProcess();
     console.log('commandLineOptions', commandLineOptions);
-    const path = commandLineOptions.open;
-    if (path) {
-      await this.openPathService.open(path);
+    const specifiedPath = commandLineOptions.open;
+    if (specifiedPath) {
+      const stats = await fsExtra.stat(specifiedPath);
+      if (stats.isFile()) {
+        const parentFolderPath = pathModule.dirname(specifiedPath);
+        await this.openPathService.open(parentFolderPath);
+        const gpsInfoExistsInSpecifiedFile = this.photoDataService.getPhotoPathsWithGpsInfo().includes(specifiedPath);
+        if (gpsInfoExistsInSpecifiedFile) {
+          this.directoryTreeViewSelectionService.select([specifiedPath]);
+        } else {
+          this.directoryTreeViewSelectionService.select([]); // Deselect all photos if the specified file does not have GPS info.
+        }
+      } else if (stats.isDirectory()) {
+        await this.openPathService.open(specifiedPath);
+      } else {
+        Logger.error(`The specified item is neither a file nor a directory, which is unexpected. Path: "${specifiedPath}"`);
+      }
     }
   }
 
