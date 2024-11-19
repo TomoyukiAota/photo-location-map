0351afa29b227792b070219d4872b7ffa84eff05
Tomoyuki Aota <tomoyukia@gmail.com>
2024-11-20 03:30:59 +0900

    Return early if specifiedPath is falsy.

--- src/app/shared/service/open-after-launch.service.ts
+++ src/app/shared/service/open-after-launch.service.ts
@@ -24,23 +24,23 @@ export class OpenAfterLaunchService {
     const commandLineOptions = await this.getCommandLineOptionsFromMainProcess();
     console.log('commandLineOptions', commandLineOptions);
     const specifiedPath = commandLineOptions.open;
-    if (specifiedPath) {
-      const stats = await fsExtra.stat(specifiedPath);
-      if (stats.isFile()) {
-        const parentFolderPath = pathModule.dirname(specifiedPath);
-        await this.openPathService.open(parentFolderPath);
-        this.photoSelectionHistoryService.reset(); // Remove the unnecessary history of selecting all photos as a result of opening the parent folder.
-        const gpsInfoExistsInSpecifiedFile = this.photoDataService.getPhotoPathsWithGpsInfo().includes(specifiedPath);
-        if (gpsInfoExistsInSpecifiedFile) {
-          this.directoryTreeViewSelectionService.select([specifiedPath]);
-        } else {
-          this.directoryTreeViewSelectionService.select([]); // Deselect all photos if the specified file does not have GPS info.
-        }
-      } else if (stats.isDirectory()) {
-        await this.openPathService.open(specifiedPath);
+    if (!specifiedPath) { return; }
+
+    const stats = await fsExtra.stat(specifiedPath);
+    if (stats.isFile()) {
+      const parentFolderPath = pathModule.dirname(specifiedPath);
+      await this.openPathService.open(parentFolderPath);
+      this.photoSelectionHistoryService.reset(); // Remove the unnecessary history of selecting all photos as a result of opening the parent folder.
+      const gpsInfoExistsInSpecifiedFile = this.photoDataService.getPhotoPathsWithGpsInfo().includes(specifiedPath);
+      if (gpsInfoExistsInSpecifiedFile) {
+        this.directoryTreeViewSelectionService.select([specifiedPath]);
       } else {
-        Logger.error(`The specified item is neither a file nor a directory, which is unexpected. Path: "${specifiedPath}"`);
+        this.directoryTreeViewSelectionService.select([]); // Deselect all photos if the specified file does not have GPS info.
       }
+    } else if (stats.isDirectory()) {
+      await this.openPathService.open(specifiedPath);
+    } else {
+      Logger.error(`The specified item is neither a file nor a directory, which is unexpected. Path: "${specifiedPath}"`);
     }
   }
 
