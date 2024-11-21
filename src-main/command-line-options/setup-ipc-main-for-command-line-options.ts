9c9fafba3abec24a14ea30e9f5a8f32a404eca12
Tomoyuki Aota <tomoyukia@gmail.com>
2024-11-22 02:14:11 +0900

    Remove unnecessary "async".

--- src-main/command-line-options/setup-ipc-main-for-command-line-options.ts
+++ src-main/command-line-options/setup-ipc-main-for-command-line-options.ts
@@ -4,7 +4,7 @@ import { Logger } from '../../src-shared/log/logger';
 import { commandLineOptionsValue } from './command-line-options-value';
 
 export function setupIpcMainForCommandLineOptions() {
-  ipcMain.handle(IpcConstants.CommandLineOptions.Get, async () => {
+  ipcMain.handle(IpcConstants.CommandLineOptions.Get, () => {
     Logger.info(`Received the IPC invoke request about getting commang line options.`);
     return commandLineOptionsValue.get();
   });
