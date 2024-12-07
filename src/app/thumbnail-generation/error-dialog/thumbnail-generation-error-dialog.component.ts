20e06f913366864f634f2f7a086d4b2b85c8cd2b
Tomoyuki Aota <tomoyukia@gmail.com>
2024-12-08 02:34:00 +0900

    Display the number of files whose thumbnail generation failed in ThumbnailGenerationErrorDialogComponent.

--- src/app/thumbnail-generation/error-dialog/thumbnail-generation-error-dialog.component.ts
+++ src/app/thumbnail-generation/error-dialog/thumbnail-generation-error-dialog.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThumbnailGenerationService } from '../service/thumbnail-generation.service';
 
 @Component({
   selector: 'app-thumbnail-generation-error-dialog',
   templateUrl: './thumbnail-generation-error-dialog.component.html',
   styleUrl: './thumbnail-generation-error-dialog.component.scss'
 })
-export class ThumbnailGenerationErrorDialogComponent {
+export class ThumbnailGenerationErrorDialogComponent implements OnInit {
+  public numberOfFilesWithoutThumbnail: number;
 
+  constructor(private thumbnailGenerationService: ThumbnailGenerationService) {
+  }
+
+  public ngOnInit() {
+    this.numberOfFilesWithoutThumbnail = this.thumbnailGenerationService.thumbnailGenerationResult.filePathsWithoutThumbnail.length;
+  }
 }
