badd28c8ff506cf52e9ad3176797983bd9dca801
Tomoyuki Aota <tomoyukia@gmail.com>
2025-04-11 02:29:42 +0900

    Remove unused functions of LatLng.

--- src/app/shared/model/lat-lng.model.ts
+++ src/app/shared/model/lat-lng.model.ts
@@ -3,26 +3,4 @@ export class LatLng {
     public latitude = 0,
     public longitude = 0) {
   }
-
-  public add(another: LatLng) {
-    if (!another)
-      return null;
-
-    return new LatLng(
-      this.latitude + another.latitude,
-      this.longitude + another.longitude
-    );
-  }
-}
-
-export function calculateCenterLatLng(latLngArray: LatLng[]) {
-  if (!latLngArray || latLngArray.length === 0)
-    return null;
-
-  const total = latLngArray.reduce((previous, current) => previous.add(current));
-  const center = new LatLng(
-    total.latitude / latLngArray.length,
-    total.longitude / latLngArray.length
-  );
-  return center;
 }
