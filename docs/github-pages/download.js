d0d4f9db82259ed91f7432ec15056fc297759b69
Tomoyuki Aota <tomoyukia@gmail.com>
2026-08-11 15:27:26 +0900

    Use Universal Binary for macOS.

--- docs/github-pages/download.js
+++ docs/github-pages/download.js
@@ -11,6 +11,7 @@ const version = '1.11.0';
 const downloadDirectory = `https://github.com/TomoyukiAota/photo-location-map/releases/download/v${version}`;
 const downloadLink = {
   win: `${downloadDirectory}/Photo-Location-Map-Setup-${version}.exe`,
+  // The macOS package is a universal build from v1.11.1 onwards, and "-universal" needs to be added to the file name below when the version above is updated to v1.11.1 or later.
   mac: `${downloadDirectory}/Photo-Location-Map-${version}.dmg`
 };
 
