9aaa9da5b36340756d611340dfe4b05214e470ca
Tomoyuki Aota <tomoyukia@gmail.com>
2026-08-12 18:02:06 +0900

    Bump download link version to 1.12.0

--- docs/github-pages/download.js
+++ docs/github-pages/download.js
@@ -7,12 +7,11 @@ const download = (url) => {
   document.body.removeChild(a);
 };
 
-const version = '1.11.0';
+const version = '1.12.0';
 const downloadDirectory = `https://github.com/TomoyukiAota/photo-location-map/releases/download/v${version}`;
 const downloadLink = {
   win: `${downloadDirectory}/Photo-Location-Map-Setup-${version}.exe`,
-  // The macOS package is a universal build from v1.11.1 onwards, and "-universal" needs to be added to the file name below when the version above is updated to v1.11.1 or later.
-  mac: `${downloadDirectory}/Photo-Location-Map-${version}.dmg`
+  mac: `${downloadDirectory}/Photo-Location-Map-${version}-universal.dmg`
 };
 
 const downloadForWindows = () => download(downloadLink.win);
